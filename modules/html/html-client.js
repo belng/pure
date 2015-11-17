62f76a1673b18daa484bcb455a3a6010ef162f1c
aravind <aravindet@gmail.com>
2015-11-17 13:55:00 +0530

    Added more stubs, postgres table structure

--- modules/html/html-client.js
+++
@@ -1,6 +0,0 @@
-let ReactDom = require("react-dom");
-
-module.exports = (core) => {
-	let App = require("./components/app")(core);
-	ReactDom.render(<App />, document.getElementById("app-container"));
-};
