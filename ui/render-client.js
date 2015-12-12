43e6b5da0c9c095aa1f018d32950aa5cf2fece4b
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-12-12 21:10:47 +0530

    Restore deleted UI files

---
+++ ui/render-client.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./components/views/Hello";
+
+document.addEventListener("readystatechange", () => {
+	if (document.readyState === "complete") {
+		ReactDOM.render(<Hello />, document.getElementById("app-container"));
+	}
+});
