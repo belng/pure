615e8238068aef3e42d5ef1220c2fc8b11c8944a
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-12-26 14:31:48 +0530

    Add routing to server

--- index-server.js
+++ index-server.js
@@ -1,40 +1,4 @@
-import koa from "koa";
-import route from "koa-route";
-import mount from "koa-mount";
-import serve from "koa-static";
-import webpack from "webpack";
-import webpackDevMiddleware from "koa-webpack-dev-middleware";
-import webpackHotMiddleware from "koa-webpack-hot-middleware";
-import React from "react";
-import ReactDOMServer from "react-dom/server";
-import ServerHTML from "./ui/ServerHTML";
-import Hello from "./ui/components/views/Hello";
-import webpackConfig from "./webpack.config";
-
-const app = koa();
-
-app.use(route.get("/", function* () {
-	this.body = "<!DOCTYPE html>" + ReactDOMServer.renderToStaticMarkup(
-		<ServerHTML
-			locale="en"
-			title="React Bolierplate"
-			description="Simple boilerplate for React"
-			body={ReactDOMServer.renderToString(<Hello radiumConfig={{ userAgent: this.headers["user-agent"] }} />)}
-		/>
-	);
-}));
-
-if (process.env.NODE_ENV === "production") {
-	app.use(mount("/dist", serve("static/dist")));
-} else {
-	const compiler = webpack(webpackConfig);
-
-	app.use(webpackDevMiddleware(compiler, {
-		publicPath: webpackConfig.output.publicPath,
-		noInfo: true
-	}));
-
-	app.use(webpackHotMiddleware(compiler));
-}
-
-app.listen(7528);
+require("source-map-support").install();
+require("babel-polyfill");
+require("babel-register");
+require("./modules/http/http-server");
--- modules/http/http-server.js
+++ modules/http/http-server.js
@@ -1,15 +1,34 @@
-let http = require("http");
+import koa from "koa";
+import route from "koa-route";
+import mount from "koa-mount";
+import serve from "koa-static";
+import webpack from "webpack";
+import webpackDevMiddleware from "koa-webpack-dev-middleware";
+import webpackHotMiddleware from "koa-webpack-hot-middleware";
+import webpackConfig from "../../webpack.config";
+import { home, room, thread } from "./routes";
 
-module.exports = (core) => {
-	let server = http.createServer((req, res) => {
-		core.emit("http/request", { req, res }, err => {
-			if (!err) return;
-			res.writeHead(500);
-			res.end(err.message);
-		});
-	});
-	
-	core.emit("http/init", server);
-	
-	server.listen(7528);
-};
+const app = koa();
+
+app.listen(7528);
+
+if (process.env.NODE_ENV === "production") {
+	// Serve files under static/dist for any requests to /dist/
+	app.use(mount("/dist", serve("static/dist")));
+} else {
+	const compiler = webpack(webpackConfig);
+
+	// Enable Webpack Dev Server
+	app.use(webpackDevMiddleware(compiler, {
+		publicPath: webpackConfig.output.publicPath,
+		noInfo: true
+	}));
+
+	// Enable Hot reloading
+	app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(route.get("/", home));
+app.use(route.get("/:room", room));
+app.use(route.get("/:room/:thread", thread));
+app.use(route.get("/:room/:thread/:title", thread));
