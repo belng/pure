67116c56878136d946b5c7b3e863d45f6fd92d0c
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-01-07 16:20:37 +0530

    create dummy db

--- modules/postgres/setup/dummy-db.js
+++ modules/postgres/setup/dummy-db.js
@@ -159,17 +159,6 @@ function repeat(fn, repeatEl) {
 		if(typeof repeatEl === 'number') {
 			next();	
 		} else {
-//			users.forEach(function(usr){
-//				for (let i=0; i<Math.floor(Math.random() * repeatEl.length); i++) {
-//					
-//					fn(usr, repeatEl[i], function(err, result) {
-//						if (err) return reject(err);
-//					})	
-//				}
-//			});
-//			
-//			resolve();
-			
 			Promise.all(users.map(usr => {
 				const promises = [];
 				
