3a4457c63974f81878d7116ff120ef6ce3f28887
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-01-19 17:35:07 +0530

    modified count module

--- modules/count/__tests__/count-test.js
+++ modules/count/__tests__/count-test.js
@@ -3,131 +3,227 @@
 jest.autoMockOff();
 
 let core = require("../../../core"),
-	count = require("../count")(core),
+	assert = require('assert'),
 	constants = require("../../../lib/constants"),
 	bus = core.bus,
 	cache = core.cache;
+//let count = require("../count");
 
-describe("increase count", () => {
-	cache.put({
-		entities: {
-			"sd23d-d23dasd-ad23-dawe": {
-				id: "sd23d-d23dasd-ad23-dawe",
-				type: constants.TYPE_TEXT,
-				parents: ["sdf87sd-sdf6-xv6-xcvx7843d", "sjfk34-sf9s-sdf43-amv-sdjfh34"],
-				body: "this is a text message 1",
-				tags: [constants.TAG_POST_STICKY],
-				createtime: Date.now(),
-				creator: "testinguser"
-			}
-		}
-	});
-	cache.put({
-		entities: {		
-			"lm23d-fd3dasd-mn23-dawe": {
-				id: "lm23d-fd3dasd-mn23-dawe",
-				type: constants.TYPE_TEXT,
-				parents: ["sdf87sd-sdf6-xv6-xcvx7843d", "sjfk34-sf9s-sdf43-amv-sdjfh34"],
-				body: "this is a text message 2",
-				tags: [constants.TAG_POST_STICKY],
-				createtime: Date.now(),
-				creator: "testinguser"
-			}
-		}
-	});
-	cache.put({
-		entities: {	
-			"sdf87sd-sdf6-xv6-xcvx7843d": {
-				id: "sdf87sd-sdf6-xv6-xcvx7843d",
-				type: constants.TYPE_THREAD,
-				parents: ["sjfk34-sf9s-sdf43-amv-sdjfh34"],
-				body: "this is a text message",
-				name: "this is a thread title",
-				tags: [constants.TAG_POST_STICKY],
-				createtime: Date.now(),
-				creator: "testinguser",
-				counts: {
-					text: 2
+describe("incriment count", () => {
+	let count = require("../count");
+	it("should add count+ to parents and user on new text", () => {
+		bus.emit("setstate", {
+			entities: {
+				"sd23d-d23dasd-ad23-dawe": {
+					id: "sd23d-d23dasd-ad23-dawe",
+					type: constants.TYPE_TEXT,
+					parents: [["sdf87sd-sdf6-xv6-xcvx7843d", "sjfk34-sf9s-sdf43-amv-sdjfh34"]],
+					body: "this is a text message 1",
+					tags: [constants.TAG_POST_STICKY],
+					createtime: Date.now(),
+					creator: "testinguser"
 				}
-			}}
+			}
+		}, (err, changes) => {
+			console.log("changes", changes.entities["testinguser"]);
+			assert.deepEqual(changes.entities["sdf87sd-sdf6-xv6-xcvx7843d"], {
+				 counts: { children: 1 },
+				 id: 'sdf87sd-sdf6-xv6-xcvx7843d',
+				 type: constants.TYPE_THREAD
+			});
+			assert.deepEqual(changes.entities["testinguser"], { counts: { texts: 1 }, id: 'testinguser' })
+		})
 	});
-	cache.put({
-		entities: {
-			"sjfk34-sf9s-sdf43-amv-sdjfh34": {
+
+	it("should add count+ to parents and user on new thread", () => {
+		bus.emit("setstate", {
+			entities: {
+				"sdf87sd-sdf6-xv6-xcvx7843d": {
+					id: "sdf87sd-sdf6-xv6-xcvx7843d",
+					type: constants.TYPE_THREAD,
+					parents: [["sjfk34-sf9s-sdf43-amv-sdjfh34"]],
+					body: "this is a text message",
+					name: "this is a thread title",
+					tags: [constants.TAG_POST_STICKY],
+					createtime: Date.now(),
+					creator: "testinguser",
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
+				counts: { children: 1 },
+			 	id: 'sjfk34-sf9s-sdf43-amv-sdjfh34',
+			 	type: 1
+			});
+			assert.deepEqual(changes.entities["testinguser"], {counts: { threads: 1 }, id: 'testinguser'});
+		})
+			
+	});
+
+	it("should add count- to parents and user on delete text", () => {
+		bus.emit("setstate", {
+			entities: {
+				"sd23d-d23dasd-ad23-dawe": {
+					id: "sd23d-d23dasd-ad23-dawe",
+					type: constants.TYPE_TEXT,
+					parents: [["sdf87sd-sdf6-xv6-xcvx7843d", "sjfk34-sf9s-sdf43-amv-sdjfh34"]],
+					body: "this is a text message 1",
+					tags: [constants.TAG_POST_STICKY],
+					deletetime: Date.now(),
+					creator: "testinguser"
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities["testinguser"]);
+			assert.deepEqual(changes.entities["sdf87sd-sdf6-xv6-xcvx7843d"], {
+				 counts: { children: -1 },
+				 id: 'sdf87sd-sdf6-xv6-xcvx7843d',
+				 type: constants.TYPE_THREAD
+			});
+			assert.deepEqual(changes.entities["testinguser"], { counts: { texts: -1 }, id: 'testinguser' })
+		})
+	});
+	
+	it("should add count- to parents and user on delete thread", () => {
+		bus.emit("setstate", {
+			entities: {
+				"sdf87sd-sdf6-xv6-xcvx7843d": {
+					id: "sdf87sd-sdf6-xv6-xcvx7843d",
+					type: constants.TYPE_THREAD,
+					parents: [["sjfk34-sf9s-sdf43-amv-sdjfh34"]],
+					body: "this is a text message",
+					name: "this is a thread title",
+					tags: [constants.TAG_POST_STICKY],
+					deletetime: Date.now(),
+					creator: "testinguser",
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
+				counts: { children: -1 },
+			 	id: 'sjfk34-sf9s-sdf43-amv-sdjfh34',
+			 	type: 1
+			});
+			assert.deepEqual(changes.entities["testinguser"], {counts: { threads: -1 }, id: 'testinguser'});
+		})
+	});
+	
+	it("should add follower count to related item", () => {
+		bus.emit("setstate", {
+			entities: {
+				"testinguser_scrollback": {
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_FOLLOWER
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
 				id: "sjfk34-sf9s-sdf43-amv-sdjfh34",
 				type: constants.TYPE_ROOM,
-				name: "scrollback",
-				body: "thsi is room dscription",
-				tags: [constants.TAG_ROOM_CITY],
-				counts: {
-					thread: 1,
-					text: 2
-				},
-				createtime: Date.now()
-			}}
-	});
-	cache.put({
-		entities: {
-			testinguser: {
-				id: "testinguser",
-				name: "Testinguser",
-				identities: ["testinguser@mailinator.com"],
-				tags: [constants.TAG_USER_EMAIL],
-				timezone: 330,
-				resources: {
-					scrollback: "online"
-				},
-				presence: "online",
-				counts: {
-					room: 1,
-					text: 2,
-					thread: 1
-				},
-				createtime: Date.now()
-			}
-		}
+				counts: {follower: 1}
+			});
+		})
 	});
 	
-	it("should add inc op to counts of all parents and user on text", () => {
-		let op = { text: 1, __op__: { text: [ 'inc' ] } };
-		bus.emit("setstate", {
+	it("should remove 1 follower count of related item", () => {
+		cache.put({
 			entities: {
-				"askdh9-fd3dasd-mn23-dawe": {
-					id: "askdh9-fd3dasd-mn23-dawe",
-					type: constants.TYPE_TEXT,
-					parents: ["sdf87sd-sdf6-xv6-xcvx7843d", "sjfk34-sf9s-sdf43-amv-sdjfh34"],
-					body: "this is a text message 3",
-					tags: [constants.TAG_POST_STICKY],
-					createtime: Date.now(),
-					creator: "testinguser"
-				},
+				"testinguser_scrollback": {
+					id: "testinguser_scrollback",
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_FOLLOWER 
+				}
 			}
-		}, () => {
-			expect(cache.getEntity("testinguser").counts.op).toEqual(op);
-			expect(cache.getEntity("sdf87sd-sdf6-xv6-xcvx7843d").counts.op).toEqual(op);
-			expect(cache.getEntity("sjfk34-sf9s-sdf43-amv-sdjfh34").counts.op).toEqual(op);
 		});
-	});
-	
-	it.only("should add inc op on thread", () => {
-		let op = { thread: 1, __op__: { thread: [ "inc" ] } };
+		
 		bus.emit("setstate", {
 			entities: {
-				"askdh9-fd3dasd-mn23-dawe": {
-					id: "askdh9-fd3dasd-mn23-dawe",
-					type: constants.TYPE_THREAD,
-					parents: ["sjfk34-sf9s-sdf43-amv-sdjfh34"],
-					body: "this is a 1st text of this thread",
-					name: "this is thread title",
-					tags: [constants.TAG_POST_STICKY],
-					createtime: Date.now(),
-					creator: "testinguser"
-				},
+				"testinguser_scrollback": {
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_NONE
+				}
 			}
-		}, () => {
-			console.log(cache.getEntity("sjfk34-sf9s-sdf43-amv-sdjfh34").counts.op)
-			expect(cache.getEntity("testinguser").counts.op).toEqual(op);
-			expect(cache.getEntity("sjfk34-sf9s-sdf43-amv-sdjfh34").counts.op).toEqual(op);
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
+				id: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+				type: constants.TYPE_ROOM,
+				counts: {follower: -1}
+			});
+		})
+	});
+	
+	it("should remove 1 follower count and add banned to related item", () => {
+		cache.put({
+			entities: {
+				"testinguser_scrollback": {
+					id: "testinguser_scrollback",
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_FOLLOWER 
+				}
+			}
+		});
+		
+		bus.emit("setstate", {
+			entities: {
+				"testinguser_scrollback": {
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_BANNED
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
+				id: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+				type: constants.TYPE_ROOM,
+				counts: {follower: -1, banned: 1}
+			});
+		})
+	});
+	
+	it("should remove 1 banned count and add follower to related item", () => {
+		cache.put({
+			entities: {
+				"testinguser_scrollback": {
+					id: "testinguser_scrollback",
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_BANNED 
+				}
+			}
+		});
+		
+		bus.emit("setstate", {
+			entities: {
+				"testinguser_scrollback": {
+					user: "testinguser",
+					item: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+					type: constants.TYPE_ROOMREL,
+					role: constants.ROLE_FOLLOWER
+				}
+			}
+		}, (err, changes) => {
+			console.log(changes.entities)
+			assert.deepEqual(changes.entities["sjfk34-sf9s-sdf43-amv-sdjfh34"], {
+				id: "sjfk34-sf9s-sdf43-amv-sdjfh34",
+				type: constants.TYPE_ROOM,
+				counts: {follower: 1, banned: -1}
+			});
 		})
 	})
+
 });
