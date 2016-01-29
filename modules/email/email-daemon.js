573b5edfb75bfdedf124accc20c3c56d3b2ea4fc
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-01-29 11:47:40 +0530

    fixes

--- modules/email/email-daemon.js
+++ modules/email/email-daemon.js
@@ -1,36 +1,39 @@
 "use strict";
 
-let pg = require("../../lib/pg"),
-	winston = require("winston"),
-	constants = require("../../lib/constants.json"),
-	conf = require("../../core").config, config = conf.email,
-	connString = "pg://" + config.pg.username + ":" + config.pg.password + "@" + 
-	config.pg.server + "/" + config.pg.db;
+import pg from "../../lib/pg";
+import winston from "winston";
+import {config, Constants} from "../../core";
+import sendWelcomeEmail from "./welcomeEmail";
+import sendMentionEmail from "./mentionEmail";
+import sendDigestEmail from "./digestEmail";
+let conf = config.email;
+let connString = "pg://" + conf.pg.username + ":" + conf.pg.password + "@" + 
+	conf.pg.server + "/" + conf.pg.db;
 
-(() => {
-	if(!config.auth) {
-		winston.info("Email module not enabled")
-		return;
-	}
-	let sendWelcomeEmail = require ("./welcomeEmail"),
-		sendMentionEmail = require("./mentionEmail"),
-		sendDigestEmail = require ("./digestEmail");
+if(!conf.auth) {
+	winston.info("Email module not enabled")
+} else {
 	pg.read(connString, {
 		$: "SELECT * FROM jobs WHERE jobid in (&(jids))",
-		jids: [/*constants.JOB_EMAIL_WELCOME, constants.JOB_EMAIL_MENTION, */constants.JOB_EMAIL_DIGEST]
+		jids: [Constants.JOB_EMAIL_WELCOME/*, Constants.JOB_EMAIL_MENTION, Constants.JOB_EMAIL_DIGEST*/]
 	}, function (err, results) {
+		winston.info(results)
 		results.forEach((row) => {
 			switch(row.jobid) {
-				case constants.JOB_EMAIL_WELCOME:
-					sendWelcomeEmail(row, config);
+				case Constants.JOB_EMAIL_WELCOME:
+					sendWelcomeEmail(row, conf);
 					break;
-				case constants.JOB_EMAIL_MENTION:
-					sendMentionEmail(row, config);
+				case Constants.JOB_EMAIL_MENTION:
+					sendMentionEmail(row, conf);
 					break;
-				case constants.JOB_EMAIL_DIGEST:
-					sendDigestEmail(row, config);
+				case Constants.JOB_EMAIL_DIGEST:
+					sendDigestEmail(row, conf);
+					break;
+				default:
+					winston.info("wrong job id")
 					break;
 			}
 		});
 	});
-})();
+}
+	
--- modules/email/welcomeEmail.js
+++ modules/email/welcomeEmail.js
@@ -2,17 +2,15 @@
 const WELCOME_INTERVAL = 5 * 60 * 1000,
 	  WELCOME_DELAY = 5 * 60 * 1000;
 
-let fs = require("fs"),
-	jwt = require("jsonwebtoken"),
-	handlebars = require("handlebars"),
-	pg = require("../../lib/pg"),
-	send = require("./sendEmail.js"),
-	constants = require("../../lib/constants"),
-	template = handlebars.compile(fs.readFileSync(__dirname + "/views/welcomeEmail.hbs", "utf-8")),
-	lastEmailSent, config = require("../../core").config.email,
-	connStr = "pg://" + config.pg.username + ":" + config.pg.password + "@" + config.pg.server + "/" + config.pg.db;
-
-
+import winston from "winston";
+import fs from "fs";
+import jwt from "jsonwebtoken";
+import handlebars from "handlebars";
+import pg from "../../lib/pg";
+import send from "./sendEmail.js";
+import {Constants} from "../../core";
+let template = handlebars.compile(fs.readFileSync(__dirname + "/views/welcomeEmail.hbs", "utf-8")),
+	lastEmailSent, config, connStr ;
 
 function initMailSending(cUserRel) {
 	let user = userRel.currentUser,
@@ -30,7 +28,6 @@ function initMailSending(cUserRel) {
 function sendWelcomeEmail () {
 	let getMailObj = require("./prepareMailObj");
 	let end = Date.now() - WELCOME_DELAY;
-	
 	pg.readStream(connStr, {
 		$: `SELECT * FROM users LEFT OUTER JOIN members
 			ON members.user = users.id WHERE identities @> '{email}'
@@ -38,7 +35,7 @@ function sendWelcomeEmail () {
 		start: lastEmailSent,
 		end: end
 	}).on("row", function (userRel) {
-
+		winston.info("sendng welocome email")
 		let emailObj = getMailObj(userRel) || {};
 		
 		if (Object.keys(emailObj).length !==0) initMailSending(emailObj);
@@ -48,14 +45,19 @@ function sendWelcomeEmail () {
 		pg.write(connString, {
 			$: "UPDATE jobs SET lastrun=&{end} WHERE jobid=&{jid}",
 			end: end,
-			jid: constants.JOB_EMAIL_WELCOME
+			jid: Constants.JOB_EMAIL_WELCOME
 		}, function (err, results) {
 			if(!err) log.i("successfully updated jobs");
 		});
 	});
 }
 
-module.exports = (row) => {
+module.exports = (row, conf) => {
+	config = conf;
+	connStr = "pg://" + config.pg.username + ":" + config.pg.password + "@" + config.pg.server + "/" + config.pg.db;
+	if(config.debug) {
+		sendWelcomeEmail()
+	}
 	lastEmailSent = row.lastrun;
 	setInterval(sendWelcomeEmail, WELCOME_INTERVAL);
 }
