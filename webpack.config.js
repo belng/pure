0dd7bf1a899437ef0c31080303b55740216b3910
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-01 06:14:17 +0530

    Use 'babel-preset-react-hmre'

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const __DEV__ = process.env.NODE_ENV !== "production";
 
 const webpack = require("webpack");
 const path = require("path");
+const fs = require("fs");
 
 const plugins = [
 	new webpack.EnvironmentPlugin("NODE_ENV"),
@@ -11,6 +12,8 @@ const plugins = [
 const entry = [
 	"./ui/Client"
 ];
+
+const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, ".babelrc"), "utf-8").toString());
 
 module.exports = {
 	devtool: "source-map",
@@ -26,15 +29,22 @@ module.exports = {
 		preLoaders: [
 			{
 				test: /\.js$/,
-				loaders: [ "eslint" ],
+				loader: "eslint",
 				exclude: /node_modules/
 			}
 		],
 		loaders: [
 			{
 				test: /\.js$/,
-				loaders: __DEV__ ? [ "react-hot", "babel" ] : [ "babel" ],
-				exclude: /node_modules/
+				loader: "babel",
+				exclude: /node_modules/,
+				query: Object.assign({}, babelrc, {
+					env: {
+						developement: {
+							presets: [ "react-hmre" ],
+						},
+					}
+				})
 			},
 			{
 				test: /\.json$/,
