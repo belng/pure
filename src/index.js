7de71ab9027dffa18938352d26b770234ebc9610
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-02-04 17:01:40 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import "source-map-support/register";
 import "babel-polyfill";
 import "./start/heyneighbor-server";
+import sourceMapSupport from "source-map-support";
+
+if (process.env.NODE_ENV === "production") {
+	sourceMapSupport.install();
+}
