de6cdf796ad56701fe5d9330139a9665ae93dfa4
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-10 19:33:01 +0530

    Accept a helper method to operate on the data

--- src/modules/store/Connect.js
+++ src/modules/store/Connect.js
@@ -4,18 +4,18 @@ import React, { Component } from 'react';
 import shallowEqual from 'shallowequal';
 import storeShape from './storeShape';
 
-export default function(mapSubscriptionToProps: Object, mapDispatchToProps: Object): Function {
+export default function(mapSubscriptionToProps: ?Object, mapActionsToProps: ?Object): Function {
 	if (process.env.NODE_ENV !== 'production') {
-		if (mapSubscriptionToProps && mapDispatchToProps) {
+		if (mapSubscriptionToProps && mapActionsToProps) {
 			for (const key in mapSubscriptionToProps) {
-				if (mapDispatchToProps[key]) {
-					throw new Error(`Prop ${key} found both in subscriptions and dispatch. Props must be unique.`);
+				if (mapActionsToProps[key]) {
+					throw new Error(`Prop ${key} found both in subscriptions and actions. Props must be unique.`);
 				}
 			}
 
-			for (const key in mapDispatchToProps) {
+			for (const key in mapActionsToProps) {
 				if (mapSubscriptionToProps[key]) {
-					throw new Error(`Prop ${key} found both in subscriptions and dispatch. Props must be unique.`);
+					throw new Error(`Prop ${key} found both in subscriptions and actions. Props must be unique.`);
 				}
 			}
 		}
@@ -43,11 +43,11 @@ export default function(mapSubscriptionToProps: Object, mapDispatchToProps: Obje
 						const sub = mapSubscriptionToProps[item];
 
 						if (!Array.isArray(sub)) {
-							throw new Error(`Invalid subscription ${item}. Subscription must be an array with first item being the key to watch, and second item as options.`);
+							throw new Error(`Invalid subscription ${item}. Subscription must be an array with the key to watch, options, and helper function.`);
 						}
 
 						this._watches.push(
-							store.watch(sub[0], sub[1], this._updateListener(item))
+							store.watch(sub[0], sub[1], this._updateListener(item, sub[2]))
 						);
 					}
 				}
@@ -67,24 +67,24 @@ export default function(mapSubscriptionToProps: Object, mapDispatchToProps: Obje
 				return !shallowEqual(this.props, nextProps) || !shallowEqual(this.state, nextState);
 			}
 
-			_updateListener = name => {
+			_updateListener = (name, helper) => {
 				return data => this.setState({
-					[name]: data
+					[name]: helper ? helper(data) : data
 				});
 			};
 
 			render() {
 				const props = { ...this.state };
 
-				if (mapDispatchToProps) {
-					for (const item in mapDispatchToProps) {
-						const dispatch = mapDispatchToProps[item](this.context.store);
+				if (mapActionsToProps) {
+					for (const item in mapActionsToProps) {
+						const action = mapActionsToProps[item](this.context.store);
 
-						if (typeof dispatch !== 'function') {
-							throw new Error(`Invalid dispatch function in ${item}. Action creators must return a curried dispatch function.`);
+						if (typeof action !== 'function') {
+							throw new Error(`Invalid action in ${item}. Action creators must return a curried action function.`);
 						}
 
-						props[item] = dispatch;
+						props[item] = action;
 					}
 				}
 
