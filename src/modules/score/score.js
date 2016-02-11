e63e1b3a2b58d5a408bad11c6b0a753672058c99
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-02-11 16:25:17 +0530

    score module added

---
+++ src/modules/score/score.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import { Constants, bus } from '../../core';
+
+function getScore(params) {
+	return Math.log(params);
+}
+bus.on('setstate', (changes, next) => {
+	if (!changes.entities) return next();
+
+	for (const id in changes.entities) {
+		const entity = changes.entities[id];
+
+		if (entity.type === Constants.TYPE_THREAD) {
+			entity.score = getScore(entity.updateTime);
+			changes.entities[id] = entity;
+		}
+
+//		if (entity.type === Constants.TYPE_TEXT) {
+//			const thread = changes.entities[entity.parents[0]];
+//
+//			thread.score = getScore(entity.createtime);
+//			changes.entities[entity.parents[0]] = thread;
+//		}
+	}
+
+	next();
+});
