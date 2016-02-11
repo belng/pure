ce4e65120b2e6995058c23754c557bc7a6e65c5c
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-11 20:20:41 +0530

    Cleanup libraries

---
+++ src/lib/Smiley.js
@@ -0,0 +1,115 @@
+/* @flow */
+
+const smileyMap = {
+	// Heart
+	'💕': '<3',
+	'💔': '</3',
+
+	// Cool
+	'😎': '8)',
+
+	// Astonished
+	'😧': 'D:',
+
+	// Monkey
+	'🐵': ':o)',
+
+	// Smile
+	'😊': [ '(:', ':)', ':-)', ':-]', ':->', ':>' ],
+	'😀': [ '=)', '=-)', ':3' ],
+
+	// Grin
+	'😂': [ ':D', ':-D' ],
+
+	// Frown
+	'😟': [ ':(', ':-(', ':-c' ],
+
+	// Wink
+	'😉': [ ';)', ';-)', ',-)', '*-)' ],
+
+	// Tongue
+	'😛': [ ':p', ':–p', ':b', ':-b' ],
+	'😜': [ ';p', ';–p', ';b', ';-b' ],
+
+	// Open mouth
+	'😱': [ ':o', ':-o', ':-()' ],
+
+	// Distorted mouth
+	'😕': [ ':/', ':-/', ':\\', ':-\\' ],
+
+	// Beaked lips
+	'😗': [ ':*', ':-*', ':-<>' ],
+	'😘': ':-@',
+
+	// Sealed lips
+	'😷': [ ':-X', ':-#' ],
+
+	// Halo
+	'😇': [ '0:-)', 'o:-)' ],
+
+	// Tear
+	'😢': [ ":'(", ":'-(" ],
+
+	// Horns
+	'😈': [ '>:-)', '>:-D' ],
+
+	// Blank
+	'😐': [ ':|', ':-|' ],
+
+	// Spittle
+	'😖': ':-p~~',
+
+	// Arrow
+	'😒': [ '>.>', '>_>' ]
+};
+
+let smileyIndex;
+
+function buildIndex() {
+	smileyIndex = {};
+
+	for (const smiley in smileyMap) {
+		if (Array.isArray(smileyMap[smiley])) {
+			for (let i = 0, l = smileyMap[smiley].length; i < l; i++) {
+				smileyIndex[smileyMap[smiley][i]] = smiley;
+			}
+		} else {
+			smileyIndex[smileyMap[smiley]] = smiley;
+		}
+	}
+}
+
+export function format(text: string): string {
+	if (!smileyIndex) {
+		buildIndex();
+	}
+
+	let formatted = '';
+
+	const lines = text.trim().split('\n');
+
+	for (let i = 0, l = lines.length; i < l; i++) {
+		const words = lines[i].split(' ');
+
+		for (let j = 0, k = words.length; j < k; j++) {
+			const part = words[j];
+			const emoji = smileyIndex[part];
+
+			formatted += (emoji ? emoji : part);
+
+			if (j < k - 1) {
+				formatted += ' ';
+			}
+		}
+
+		if (i < l - 1) {
+			formatted += '\n';
+		}
+	}
+
+	return formatted;
+}
+
+export function isEmoji(c: string): boolean {
+	return /^(\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDE4F])$/i.test(c);
+}
--- src/lib/extract.js
+++ src/lib/extract.js
@@ -1,11 +1,13 @@
-/* Takes a server-side message and converts it to an on-the-
-   wire message suitable for sending to server.
+/*
+ * Takes a server-side message and converts it to an on-the-
+ * wire message suitable for sending to server.
+ *
+ * Equivalent to the censor app but cleaner;
+ * Creates a new object containing only non-sensitive fields,
+ * leaving the original unchanged.
+ *
+ * To be implemented.
+ * @flow
+ */
 
-   Equivalent to the censor app but cleaner;
-   Creates a new object containing only non-sensitive fields,
-   leaving the original unchanged.
-
-   To be implemented.
-*/
-
-export default message => message;
+export default (message: Object): Object => message;
