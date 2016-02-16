c3404c9c0b806b0dde04fd7e621dafb4b3241558
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-02-16 17:27:24 +0530

    lint fixes

--- src/modules/email/digestEmail.js
+++ src/modules/email/digestEmail.js
@@ -1,6 +1,4 @@
-'use strict';
-const DIGEST_INTERVAL = 60 * 60 * 1000, DIGEST_DELAY = 24 * 60 * 60 * 1000;
-
+import getMailObj from './buildMailObj';
 import { Constants, config } from '../../core';
 import log from 'winston';
 import fs from 'fs';
@@ -9,19 +7,21 @@ import pg from '../../lib/pg';
 import jwt from 'jsonwebtoken';
 import send from './sendEmail';
 import Counter from '../../lib/counter';
+const conf = config.email, DIGEST_INTERVAL = 60 * 60 * 1000, DIGEST_DELAY = 24 * 60 * 60 * 1000,
+	  template = handlebars.compile(fs.readFileSync(__dirname + '/views/' + config.appName + '.digest.hbs', 'utf-8'));
 
-let conf = config.email, lastEmailSent,
-	connStr = 'pg://' + config.pg.username + ':' + config.pg.password + '@' + config.pg.server + '/' + config.pg.db,
-	template = handlebars.compile(fs.readFileSync(__dirname + '/views/' + config.appName + '.digest.hbs', 'utf-8'));
+let lastEmailSent;
+
 
 function getSubject(rels) {
-	var counts = rels.length - 1, heading = '';
-	heading = '[' + rels[0].room + '] ' + rels[0].threads[0].threadTitle + ' +' + counts + ' more';
+	const counts = rels.length - 1;
+	const heading = '[' + rels[0].room + '] ' + rels[0].threads[0].threadTitle + ' +' + counts + ' more';
+
 	return heading;
 }
 
 function initMailSending (userRel) {
-	let user = userRel.currentUser,
+	const user = userRel.currentUser,
 		rels = userRel.currentRels,
 		emailAdd = user.identities.filter((ident) => ident.indexOf('mailto:') === 0),
 		emailHtml = template({
@@ -30,53 +30,57 @@ function initMailSending (userRel) {
 			rooms: rels
 		}),
 		emailSub = getSubject(rels);
-	console.log(emailSub);
+
+//	console.log(emailSub);
 	send(conf.from, emailAdd, emailSub, emailHtml);
 }
 
 function sendDigestEmail () {
-	let getMailObj = require('./buildMailObj'),
-		startPoint = Date().now - 2 * DIGEST_DELAY,
-		start = lastEmailSent < startPoint ? lastEmailSent : startPoint,
-		end = Date.now() - DIGEST_DELAY;
-	let counter = new Counter();
+
+	const startPoint = Date().now - 2 * DIGEST_DELAY,
+		counter = new Counter();
+
+	let end = Date.now() - DIGEST_DELAY,
+		start = lastEmailSent < startPoint ? lastEmailSent : startPoint;
 
 	function getTimezone(hour) {
-		let UtcHrs = new Date().getUTCHours(),
+		const UtcHrs = new Date().getUTCHours(),
 			c = UtcHrs > 12 ? 24 - UtcHrs : UtcHrs;
 
-		let d = c > hour ? c - hour : hour - c,
+		const d = c > hour ? c - hour : hour - c,
 			tz = d * 60;
 
-		let tzMin = tz - 30,
+		const tzMin = tz - 30,
 			tzMax = tz + 30;
+
 		return { min: tzMin, max: tzMax };
 	}
 
 
-	let tz = getTimezone(conf.digestEmailTime);
-	console.log(tz);
+	const tz = getTimezone(conf.digestEmailTime);
+
+//	console.log(tz);
 	if (conf.debug) {
-		start = 0, end = Date.now(), tz.min = 0, tz.max = 1000;
+		start = 0; end = Date.now(); tz.min = 0; tz.max = 1000;
 	}
 
-	/*WITH
+	/* WITH
               urel AS (WITH u AS (SELECT * FROM users, roomrelations rr WHERE rr.user=users.id AND role >= &{follower}
 	          AND presencetime > &{start} AND presencetime < &{end} AND timezone >= &{min} AND timezone < &{max})
               SELECT u.id uid, rooms.name rname, * FROM u, rooms WHERE u.item=rooms.id)
             SELECT urel.uid uid, urel.rname rname, threads.name title, * FROM threads, urel where urel.item = threads.parents[1]
-            AND threads.updateTime > urel.presencetime order by uid*/
+            AND threads.updateTime > urel.presencetime order by uid */
 
-	pg.readStream(connStr, {
-		$: `with urel as (select rrls.presencetime ptime, * from users join roomrels rrls on users.id=rrls.user where roles @> '{3}' and rrls.presencetime < &{end} and timezone >= &{min} and timezone < &{max}) select * from urel join threads on threads.parents[1]=urel.item order by urel.id`, //where threads.createtime > urel.ptime
-		start: start,
-		end: end,
+	pg.readStream(config.connStr, {
+		$: `with urel as (select rrls.presencetime ptime, * from users join roomrels rrls on users.id=rrls.user where roles @> '{3}' and rrls.presencetime < &{end} and timezone >= &{min} and timezone < &{max}) select * from urel join threads on threads.parents[1]=urel.item order by urel.id`, // where threads.createtime > urel.ptime
+		start,
+		end,
 		follower: Constants.ROLE_FOLLOWER,
 		min: tz.min,
 		max: tz.max
 	}).on('row', (urel) => {
 		counter.inc();
-		pg.read(connStr, {
+		pg.read(config.connStr, {
 			$: `select * from rooms where id=&{id} `, //and presencetime<&{roletime}
 			id: urel.parents[0]
 		}, (err, room) => {
@@ -91,7 +95,7 @@ function sendDigestEmail () {
 			counter.then(() => {
 				let c = getMailObj({});
 				initMailSending(c);
-				pg.write(connStr, [ {
+				pg.write(config.connStr, [ {
 					$: 'UPDATE jobs SET lastrun=&{end} WHERE jobid=&{jid}',
 					end: end,
 					jid: Constants.JOB_EMAIL_MENTION
---
+++ src/modules/email/unsubscribe.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import jwt from 'jsonwebtoken';
+import log from 'winston';
+import { bus, config } from '../../core';
+
+const template = fs.readFileSync(__dirname + '/views/unsub.html', 'utf8');
+
+function handleRequest (req, res) {
+	let decoded, emailAddress;
+
+	// extract the Email address from the request
+	try {
+		decoded = jwt.verify(req.query.email, config.secret);
+
+	} catch (e) {
+		log.i('Invalid unsubscribe JWT: ' + req.query.email);
+		res.end(template.replace('${message}', 'You were not unsubscribed because the unsubscribe link has expired. Please click the link on a newer email.'));
+	}
+	emailAddress = decoded.email;
+
+	// search the database and find the user with this email
+	bus.emit('getUsers', {
+		identity: 'mailto:' + emailAddress,
+		session: 'internal/unsubscribe'
+	}, (err, query) => {
+		if (err) {
+			return res.end(template.replace('${message}', 'Sorry, an internal server error prevented you from being unsubscribed.'));
+		}
+
+		if (!query.results || !query.results.length) {
+			return res.end(template.replace('${message}', 'User does not exist.'));
+		}
+
+		// Received the user from the database! Changing the settings...
+		const user = query.results[0];
+
+		user.params.email = user.params.email || {};
+		user.params.email.frequency = 'never';
+		user.params.email.notifications = false;
+
+
+		// Saving the saved settings back into the database.
+		bus.emit('user', {
+			to: user.id,
+			user,
+			session: 'internal/unsubscribe'
+		}, (e) => {
+			if (e) {
+				res.end(template.replace('${message}', ' Sorry, an internal server error prevented you from being unsubscribed.'));
+				return;
+			}
+			res.header('Content-Type', 'text/html');
+			res.end(template.replace('${message}', 'You have been unsubscribed'));
+		});
+	});
+}
+
+bus.on('http/init', (payload) => {
+	payload.push({
+		get: {
+			'/r/unsubscribe': handleRequest
+		}
+	});
+});
