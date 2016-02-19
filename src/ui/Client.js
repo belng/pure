7ee194d2adb4c61e5909f024526954338499923e
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-19 22:15:19 +0530

    Provide store to the client

--- src/ui/Client.js
+++ src/ui/Client.js
@@ -2,13 +2,21 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/views/Home.web';
+import Provider from '../modules/store/Provider';
+import * as store from '../modules/store/store';
 import '../modules/socket/socket-client';
 
 if (process.env.NODE_ENV !== 'production') {
 	require('../modules/client-test/client-test');
 }
+
 document.addEventListener('readystatechange', () => {
 	if (document.readyState === 'complete') {
-		ReactDOM.render(<Home />, document.getElementById('root'));
+		ReactDOM.render(
+			<Provider store={store}>
+				<Home />
+			</Provider>,
+			document.getElementById('root')
+		);
 	}
 });
