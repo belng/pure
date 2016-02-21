20059298b3d83dd101e07500d0c3edbade06103f
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-21 14:37:09 +0530

    Annotate URL library with Flow

--- src/lib/URL.js
+++ src/lib/URL.js
@@ -1,16 +1,18 @@
-export function isValidURL(link) {
+/* @flow */
+
+export function isValidURL(link: string): boolean {
 	return /^((https?|ftp):\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i.test(link);
 }
 
-export function isValidMail(link) {
+export function isValidMail(link: string): boolean {
 	return /^(mailto:)?[^@]+@[^@]+\.[^@]+$/i.test(link);
 }
 
-export function isValidTel(link) {
+export function isValidTel(link: string): boolean {
 	return /^(tel:)?(?:\+?(\d{1,3}))?[-.\s(]*(\d{3})?[-.\s)]*(\d{3})[-.\s]*(\d{4})(?: *x(\d+))?$/.test(link);
 }
 
-export function buildLink(link) {
+export function buildLink(link: string): ?string {
 	if (isValidURL(link)) {
 		// a normal link
 		return /^(https?|ftp):\/\//.test(link) ? link : 'http://' + link;
@@ -29,16 +31,14 @@ export function buildLink(link) {
 	return null;
 }
 
-export function parseURLs(text, count) {
+export function parseURLs(text: string, count?: number): Array<string> {
 	const links = [];
 	const words = text.replace(/(\r\n|\n|\r)/g, ' ').split(' ');
 
-	let url;
-
 	for (let i = 0, l = words.length; i < l; i++) {
-		url = buildLink(words[i].trim().replace(/[\.,\?!:;]+$/, ''));
+		const url = buildLink(words[i].trim().replace(/[\.,\?!:;]+$/, ''));
 
-		if (/^https?:\/\//.test(url)) {
+		if (url && /^https?:\/\//.test(url)) {
 			links.push(url);
 
 			if (count && links.length >= count) {
