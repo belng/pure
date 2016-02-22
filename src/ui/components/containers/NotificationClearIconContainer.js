38c2ae3dfbec61d52bbd241e2b5c379abcc685cf
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-22 17:53:02 +0530

    Add container for NotificationClearIcon

--- src/ui/components/containers/NotificationClearIconContainer.js
+++ src/ui/components/containers/NotificationClearIconContainer.js
@@ -1,3 +1,11 @@
 /* @flow */
 
-// TODO
+import Connect from '../../../modules/store/Connect';
+import Dummy from '../views/Dummy';
+import { dismissAllNotes } from '../../../modules/store/actions';
+
+const NotificationClearIconContainer = Connect(null, {
+	dismissAllNotes: (props, store) => () => store.setState(dismissAllNotes())
+})(Dummy);
+
+export default NotificationClearIconContainer;
