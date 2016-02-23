d5ec71ecc7db049c5bc2a0fc18c6c2b904500549
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-23 20:52:54 +0530

    Add native modules and type definitions

---
+++ src/ui/modules/Geolocation.android.js
@@ -0,0 +1,46 @@
+import { NativeModules } from 'react-native';
+import RCTDeviceEventEmitter from 'RCTDeviceEventEmitter';
+
+const { GeolocationModule } = NativeModules;
+
+const watchCallbacks = {};
+
+let watchID = 0;
+
+RCTDeviceEventEmitter.addListener('geolocationChange', ev => {
+	for (const id in watchCallbacks) {
+		watchCallbacks[id](ev);
+	}
+});
+
+export default {
+	getCurrentPosition() {
+		return GeolocationModule.getCurrentPosition();
+	},
+
+	watchPosition(success) {
+		GeolocationModule.startWatching();
+
+		watchID++;
+
+		watchCallbacks[watchID] = success;
+
+		return watchID;
+	},
+
+	clearWatch(id) {
+		delete watchCallbacks[id];
+
+		if (Object.keys(watchCallbacks).length === 0) {
+			GeolocationModule.stopWatching();
+		}
+	},
+
+	isGPSEnabled() {
+		return GeolocationModule.isGPSEnabled();
+	},
+
+	showGPSSettings() {
+		GeolocationModule.showGPSSettings();
+	}
+};
---
+++ src/ui/modules/Share.js.flow
@@ -0,0 +1,5 @@
+/* @flow */
+
+export default class Share {
+	static shareItem: (title: string, content: string) => Promise<boolean>;
+}
