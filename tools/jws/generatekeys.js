466d22c98837457d4bbfd3728d3fa6b3d52f4259
harish <harry.softer@gmail.com>
2016-02-23 15:15:44 +0530

    Add generatekeys dependencies

--- tools/jws/generatekeys.js
+++ tools/jws/generatekeys.js
@@ -1,3 +1,6 @@
-var secureRandom = require("secure-random");
-var key = secureRandom(128, {type:"Buffer"}).toString("base64");
-console.log(key);
+#! ./node_modules/babel-cli/bin/babel-node.js
+
+/* eslint-disable no-console*/
+import secureRandom from 'secure-random';
+
+console.log(secureRandom(128, { type: 'Buffer' }).toString('base64'));
