ccb810b1f28a9bc4ef3459709004a8c87f5057fa
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-24 14:58:50 +0530

    Add Native code

---
+++ android/app/src/main/java/io/scrollback/neighborhoods/modules/gcm/PushNotificationPackage.java
@@ -0,0 +1,31 @@
+package io.scrollback.neighborhoods.modules.gcm;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PushNotificationPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Arrays.<NativeModule>asList(
+                new PushNotificationModule(reactContext)
+        );
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
