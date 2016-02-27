632e64df6326713cd283a2f01aaf637538b1d0fd
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-02-27 17:35:21 +0530

    Support attaching data to an error object

--- src/lib/EnhancedError.js
+++ src/lib/EnhancedError.js
@@ -2,13 +2,15 @@
 
 export default class EnhancedError extends Error {
 	code: string;
+	data: any;
 
-	constructor(message: string, code: string) {
+	constructor(message: string, code: string, data: any) {
 		super(message);
 
 		this.name = this.constructor.name;
 		this.message = message;
 		this.code = code;
+		this.data = data;
 
 		if (typeof Error.captureStackTrace === 'function') {
 			Error.captureStackTrace(this, this.constructor.name);
