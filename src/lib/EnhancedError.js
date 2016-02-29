00f25bba79b969b340cbc726b133ffce04cb29c1
harish <harry.softer@gmail.com>
2016-02-29 15:38:52 +0530

    Adding error levels for the EnhancedError

--- src/lib/EnhancedError.js
+++ src/lib/EnhancedError.js
@@ -2,14 +2,16 @@
 
 export default class EnhancedError extends Error {
 	code: string;
+	level: ?number;
 	data: ?any;
 
-	constructor(message: string, code: string, data?: any) {
+	constructor(message: string, code: string, level?: number = 0, data?: any) {
 		super(message);
 
 		this.name = this.constructor.name;
 		this.message = message;
 		this.code = code;
+		this.level = level;
 		this.data = data;
 
 		if (typeof Error.captureStackTrace === 'function') {
