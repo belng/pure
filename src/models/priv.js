a77f32bc470f18e9d203e6a1d776884ad2a80081
aravind <aravindet@gmail.com>
2016-03-03 17:56:29 +0530

    Add priv model

---
+++ src/models/priv.js
@@ -0,0 +1,13 @@
+import Item from './item';
+import { TYPE_ITEM, TYPE_PRIV } from '../lib/Constants';
+
+export default class Priv extends Item {
+	constructor(data) {
+		if (!data.type || data.type === TYPE_ITEM) {
+			data.type = TYPE_PRIV;
+		} else {
+			throw new Error('INVALID_TYPE');
+		}
+		super(data);
+	}
+}
