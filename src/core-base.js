98b16e5c63d4b1149b0b3180a107fb2754473050
harish <harry.softer@gmail.com>
2016-03-04 13:36:32 +0530

    Add is and id functions for cache

--- src/core-base.js
+++ src/core-base.js
@@ -15,6 +15,52 @@ export type Bus = {
 export { Constants };
 export const bus: Bus = new Ebus();
 export const cache = new SbCache({
-	// TODO: add is, id functions!
+	is: (entity, type) => {
+		switch (type) {
+
+		case 'thread':
+			return entity.type === Constants.TYPE_THREAD;
+		case 'text':
+			return entity.type === Constants.TYPE_TEXT;
+		case 'user':
+			return entity.type === Constants.TYPE_USER;
+		case 'room':
+			return entity.type === Constants.TYPE_ROOM;
+		case 'note':
+			return entity.type === Constants.TYPE_NOTE;
+		case 'topic':
+			return entity.type === Constants.TYPE_TOPIC;
+		case 'rel':
+			return entity.type === Constants.TYPE_REL;
+		case 'textrel':
+			return entity.type === Constants.TYPE_TEXTREL;
+		case 'threadrel':
+			return entity.type === Constants.TYPE_THREADREL;
+		case 'topicrel':
+			return entity.type === Constants.TYPE_TOPICREL;
+		case 'userrel':
+			return entity.type === Constants.TYPE_USERREL;
+		}
+
+		return false;
+	},
+	id: (entity) => {
+		switch (entity.type) {
+		case Constants.TYPE_THREAD:
+		case Constants.TYPE_TEXT:
+		case Constants.TYPE_USER:
+		case Constants.TYPE_ROOM:
+		case Constants.TYPE_NOTE:
+		case Constants.TYPE_TOPIC:
+			return entity.id;
+		case Constants.TYPE_REL:
+		case Constants.TYPE_TEXTREL:
+		case Constants.TYPE_THREADREL:
+		case Constants.TYPE_TOPICREL:
+		case Constants.TYPE_USERREL:
+			return entity.user + '_' + entity.item;
+		}
+		return entity.id;
+	},
 	entityOp: { counts: { __all__: 'inc' } }
 });
