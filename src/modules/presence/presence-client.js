fbf800416f049442510badc785e2a2e669a51927
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-06 15:59:55 +0530

    Add presence client

---
+++ src/modules/presence/presence-client.js
@@ -0,0 +1,62 @@
+/* @flow */
+
+import { bus } from '../../core-client';
+import { subscribe, on } from '../store/store';
+import { setPresence } from '../store/actions';
+
+let subscription;
+
+subscribe({ type: 'state', path: 'connectionStatus' }, status => {
+	if (status === 'online') {
+		if (subscription) {
+			subscription.remove();
+		}
+
+		subscription = subscribe({ type: 'state', path: 'user' }, id => {
+			if (id) {
+				bus.emit('change', setPresence(id, 'online'));
+				subscription.remove();
+			}
+		});
+	}
+});
+
+on('subscribe', options => {
+	if (options.slice) {
+		const { slice } = options;
+
+		switch (slice.type) {
+		case 'thread':
+			// TODO
+			break;
+		case 'text':
+			// TODO
+			break;
+		case 'rel':
+			if (slice.link === 'item') {
+				// TODO
+			}
+			break;
+		}
+	}
+});
+
+on('unsubscribe', options => {
+	if (options.slice) {
+		const { slice } = options;
+
+		switch (slice.type) {
+		case 'thread':
+			// TODO
+			break;
+		case 'text':
+			// TODO
+			break;
+		case 'rel':
+			if (slice.link === 'item') {
+				// TODO
+			}
+			break;
+		}
+	}
+});
