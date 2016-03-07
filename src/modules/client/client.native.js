fd87c291d7ad440111b193a668d886bcf025f19e
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-07 17:16:23 +0530

    Update React Native dependency

--- src/modules/client/client.native.js
+++ src/modules/client/client.native.js
@@ -1,6 +1,10 @@
 /* @flow */
 /* eslint-disable import/no-commonjs */
 
+import 'core-js/es6/symbol';
+import 'core-js/es6/array';
+import './polyfills/babelHelpers';
+
 global.navigator.userAgent = 'react-native';
 
 require('./client-base');
