2fa8ee8bbc29caf13e454868a620f2c2dc35f0f6
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-07 19:08:58 +0530

    Add more polyfills from core-js

--- src/modules/client/client.native.js
+++ src/modules/client/client.native.js
@@ -2,7 +2,9 @@
 /* eslint-disable import/no-commonjs */
 
 import 'core-js/es6/symbol';
+import 'core-js/es6/object';
 import 'core-js/es6/array';
+import 'core-js/es6/function';
 
 global.navigator.userAgent = 'react-native';
 
