e69bfe3981bc7209076e3333f0a3e4202bcc5e63
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-10 16:30:47 +0530

    Fix the Text model

--- src/models/text.js
+++ src/models/text.js
@@ -3,11 +3,12 @@ import { TYPE_ITEM, TYPE_TEXT } from '../lib/Constants';
 
 export default class Text extends Item {
 	constructor(data) {
-		if (!data.type || data.type === TYPE_ITEM) {
-			data.type = TYPE_TEXT;
-		} else {
+		if (data.type && data.type !== TYPE_ITEM && data.type !== TYPE_TEXT) {
 			throw new Error('INVALID_TYPE');
+		} else {
+			data.type = TYPE_TEXT;
 		}
+
 		super(data);
 	}
 }
