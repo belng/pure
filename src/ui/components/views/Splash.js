eaa667ad7531afb13a42e37a4f1f53e6f0b362fd
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-10 18:54:32 +0530

    Remove scrollback logo from Splash

--- src/ui/components/views/Splash.js
+++ src/ui/components/views/Splash.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import ReactNative from 'react-native';
 import shallowEqual from 'shallowequal';
 import Colors from '../../Colors';
-import AppText from './AppText';
 import Loading from './Loading';
 
 const {
@@ -23,26 +22,14 @@ const styles = StyleSheet.create({
 	logo: {
 		flex: 1,
 		resizeMode: 'contain',
-		marginTop: 180
+		marginTop: 240
 	},
 	loading: {
 		height: 24,
 		width: 24,
+		marginVertical: 96,
 		marginHorizontal: 16,
-		marginVertical: 32
 	},
-	attribution: {
-		alignItems: 'center',
-		alignSelf: 'stretch',
-		margin: 16
-	},
-	by: {
-		color: Colors.fadedWhite,
-		paddingHorizontal: 4
-	},
-	scrollback: {
-		resizeMode: 'contain'
-	}
 });
 
 export default class Splash extends Component<void, any, void> {
@@ -55,10 +42,6 @@ export default class Splash extends Component<void, any, void> {
 			<View style={styles.container}>
 				<Image style={styles.logo} source={require('../../../../assets/logo.png')} />
 				<Loading style={styles.loading} />
-				<View style={styles.attribution}>
-					<AppText style={styles.by}>by</AppText>
-					<Image style={styles.scrollback} source={require('../../../../assets/scrollback_logo.png')} />
-				</View>
 			</View>
 		);
 	}
