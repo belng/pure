bb101f3fd5a1585d237b947ef7a00a06ad845708
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-16 13:03:56 +0530

    Initialize state after listeners are added

--- src/modules/client/client-base.js
+++ src/modules/client/client-base.js
@@ -2,15 +2,15 @@
 
 import './polyfills/requestIdleCallback';
 
-import '../store/stateManager';
-import '../store/storeHelpers';
-
 import '../gcm/gcm-client';
 import '../history/history-client';
 import '../location/location-client';
 import '../presence/presence-client';
 import '../session/session-client';
 import '../socket/socket-client';
+
+import '../store/storeHelpers';
+import '../store/stateManager';
 
 if (process.env.NODE_ENV !== 'production') {
 	require('../client-test/client-test');
