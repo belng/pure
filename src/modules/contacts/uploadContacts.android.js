eebe1a34cf4dcf299e2bd3de4a4ca0d369da7f67
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-03-25 22:31:07 +0530

    Upload contacts during signup

---
+++ src/modules/contacts/uploadContacts.android.js
@@ -0,0 +1,22 @@
+/* @flow */
+
+import { config, cache } from '../../core-client';
+import Contacts from '../../ui/modules/Contacts';
+
+const {
+	protocol,
+	host
+} = config.server;
+const endpoint = `${protocol}//${host}/m/contacts`;
+
+export default function uploadContacts() {
+	const session = cache.getState('session');
+
+	if (session) {
+		Contacts.sendContacts(endpoint, {
+			auth: {
+				session
+			}
+		});
+	}
+}
--- src/ui/components/views/Onboard/GetStarted.js
+++ src/ui/components/views/Onboard/GetStarted.js
@@ -1,12 +1,15 @@
 /* @flow */
 
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactNative from 'react-native';
 import at from 'lodash/at';
 import NextButton from './NextButton';
 import StatusbarWrapper from '../StatusbarWrapper';
 import OnboardTitle from './OnboardTitle';
 import OnboardParagraph from './OnboardParagraph';
+import TouchFeedback from '../TouchFeedback';
+import Icon from '../Icon';
+import AppText from '../AppText';
 import Colors from '../../../Colors';
 
 const {
@@ -34,44 +37,104 @@ const styles = StyleSheet.create({
 	image: {
 		margin: 8,
 	},
+
+	checkboxContainer: {
+		paddingVertical: 16,
+		paddingHorizontal: 8,
+	},
+
+	checkbox: {
+		color: Colors.grey,
+	},
+
+	checkboxActive: {
+		color: Colors.info,
+	},
+
+	invite: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+
+	inviteText: {
+		color: Colors.darkGrey
+	}
 });
 
-const GetStarted = (props: { submitGetStarted: Function }) => {
-	const places = at(props, [ 'user.params.places' ])[0];
-	const completed = places ? Object.keys(places).length !== 0 : false;
+type Props = {
+	onChangeField: (type: string, value: boolean) => void;
+	submitGetStarted: Function;
+	fields: {
+		invite: { value: boolean };
+	};
+	user: ?{
+		profile: {
+			places: Array<string>;
+		};
+	};
+}
 
-	return (
-		<View style={styles.container}>
-			<StatusbarWrapper />
-			<View style={[ styles.container, styles.inner ]}>
-				<OnboardTitle style={styles.text}>
-					{
-						completed ?
-						'You are all set!' :
-						'Join the Open House'
-					}
-				</OnboardTitle>
-				<Image style={styles.image} source={require('../../../../../assets/open-door.png')} />
-				<OnboardParagraph style={styles.text}>
-					{
-						completed ?
-						'Have fun and help make your neighbourhood better.' :
-						'We are coming to your city soon! Stay connected by joining the open house group.'
-					}
-				</OnboardParagraph>
-			</View>
-			<NextButton label="Let's go" onPress={props.submitGetStarted} />
-		</View>
-	);
-};
-
-GetStarted.propTypes = {
-	submitGetStarted: PropTypes.func.isRequired,
-	user: PropTypes.shape({
-		profile: PropTypes.shape({
-			places: PropTypes.arrayOf(PropTypes.string)
+export default class GetStarted extends Component<void, Props, void> {
+	static propTypes = {
+		onChangeField: PropTypes.func.isRequired,
+		submitGetStarted: PropTypes.func.isRequired,
+		fields: PropTypes.shape({
+			invite: PropTypes.shape({
+				value: PropTypes.bool
+			})
+		}).isRequired,
+		user: PropTypes.shape({
+			profile: PropTypes.shape({
+				places: PropTypes.arrayOf(PropTypes.string)
+			})
 		})
-	})
-};
+	};
 
-export default GetStarted;
+	_handleInvitePress: Function = () => {
+		this.props.onChangeField('invite', !this.props.fields.invite.value);
+	};
+
+	render() {
+		const {
+			invite
+		} = this.props.fields;
+		const places = at(this.props, [ 'user.params.places' ])[0];
+		const completed = places ? Object.keys(places).length !== 0 : false;
+
+		return (
+			<View style={styles.container}>
+				<StatusbarWrapper />
+				<View style={[ styles.container, styles.inner ]}>
+					<OnboardTitle style={styles.text}>
+						{
+							completed ?
+							'You are all set!' :
+							'Join the Open House'
+						}
+					</OnboardTitle>
+					<Image style={styles.image} source={require('../../../../../assets/open-door.png')} />
+					<OnboardParagraph style={styles.text}>
+						{
+							completed ?
+							'Have fun and help make your neighbourhood better.' :
+							'We are coming to your city soon! Stay connected by joining the open house group.'
+						}
+					</OnboardParagraph>
+					<View style={styles.invite}>
+						<TouchFeedback onPress={this._handleInvitePress} borderless>
+							<View style={styles.checkboxContainer}>
+								<Icon
+									style={invite.value ? styles.checkboxActive : styles.checkbox}
+									name={invite.value ? 'check-box' : 'check-box-outline-blank'}
+									size={24}
+								/>
+							</View>
+						</TouchFeedback>
+						<AppText style={styles.inviteText}>Invite my friends</AppText>
+					</View>
+				</View>
+				<NextButton label="Let's go" onPress={this.props.submitGetStarted} />
+			</View>
+		);
+	}
+}
