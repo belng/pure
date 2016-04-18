5c52239cf571280f376b3fc02dd50e3e8c7eced3
Anup-Allamsetty <allamsetty.anup@gmail.com>
2016-04-18 13:44:17 +0530

    Added linking to the android emulator.

--- src/modules/modui/TodoItem.js
+++ src/modules/modui/TodoItem.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
+import { Constants } from '../../lib/Constants';
 
 const styles = {
 	name: {
 		fontSize: 14,
-		border: '0.2 ridge',
 		fontWeight: 'bold',
 		fontFamily: 'Verdana',
+		display: 'block',
 	},
 	body: {
 		fontSize: 13,
-		border: '0.2 solid',
-		fontWeight: 'ridge',
+		fontWeight: 'solid',
 		fontFamily: 'Verdana',
+		display: 'block',
 	},
 	creator: {
 		fontSize: 12,
-		border: '0.2 ridge',
 		fontWeight: 'normal',
 		fontFamily: 'Verdana',
+		display: 'block',
 	},
 	message: {
 		width: '50%',
-		border: '0.2px ridge',
-		position: 'absolute',
+		border: '0.2px solid',
 	},
 	button: {
 		width: 4,
@@ -34,11 +34,18 @@ const styles = {
 
 class TodoItem extends Component {
 	render() {
+		const todo = this.props.todo,
+			thread = (todo.type === Constants.TYPE_THREAD),
+			url = 'https://app.heyneighbor.chat/' + todo.parents[0] +
+						(thread ? '/' + todo.parents[1] : '');
+
 		return (
 			<div style={styles.message}>
-				<div style={styles.name}>{this.props.todo.name}</div>
-				<div style={styles.body}>{this.props.todo.body}</div>
-				<div style={styles.creator}>{this.props.todo.creator}</div>
+				<a href={url}>
+					<span style={styles.name}>{todo.name}</span>
+					<span style={styles.body}>{todo.body}</span>
+					<span style={styles.creator}>{todo.creator}</span>
+				</a>
 			</div>
 		);
 	}
