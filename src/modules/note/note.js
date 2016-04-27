004a3a1ed0d05c7d693ed8854cb0c82ac02fb73b
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-04-27 19:35:17 +0530

    refactor(note): simplify note module implementation

--- src/modules/note/note.js
+++ src/modules/note/note.js
@@ -1,86 +1,134 @@
-/* eslint no-loop-func: 0 */
-import log from 'winston';
-import Counter from '../../lib/counter';
-import { note as Note } from '../../models/models';
-import { Constants, bus, cache, config } from '../../core-server';
+/* @flow */
+
+import {
+	APP_PRIORITIES,
+	TYPE_NOTE,
+	TYPE_TEXTREL,
+	NOTE_MENTION,
+	ROLE_MENTIONED,
+} from '../../lib/Constants';
+import promisify from '../../lib/promisify';
 import { convertRouteToURL } from '../../lib/Route';
+import { note as Note } from '../../models/models';
+import { bus, cache, config } from '../../core-server';
+import type {
+	Note as NoteType,
+	Text as TextType,
+	Thread as ThreadType,
+	Room as RoomType,
+	User as UserType,
+} from '../../lib/schemaTypes';
 
-bus.on('change', (changes, next) => {
-	if (!changes.entities) {
-		next();
-		return;
+const server = `${config.server.protocol}//${config.server.host}`;
+
+const getEntityAsync = promisify(cache.getEntity.bind(cache));
+
+function getEntities(ids: Array<string | null>): Array<Object | null> {
+	return ids.map(id => id ? getEntityAsync(id) : null);
+}
+
+async function getItemsData(relation): Promise<{
+	creator: ?UserType;
+	text: ?TextType;
+	room: ?RoomType;
+	thread: ?ThreadType;
+}> {
+	let creator, room, thread;
+
+	const text = await getEntityAsync(relation.item);
+
+	if (text && text.parents) {
+		[
+			thread,
+			room,
+			creator,
+		] = await getEntities([ text.parents[0], text.parents[1], text.creator ]);
 	}
-	const counter = new Counter();
-	const counter1 = new Counter();
-	for (const id in changes.entities) {
-		const entity = changes.entities[id];
-		if (entity.type === Constants.TYPE_TEXTREL) {
-			if (entity.roles.indexOf(Constants.ROLE_MENTIONED) === -1) {
-				continue;
-			}
-			counter1.inc();
-			let item = changes.entities[entity.item], roomName;
-			const now = Date.now(),
-				noteObj = {
-					group: '',
-					user: entity.user,
-					event: Constants.NOTE_MENTION,
-					createTime: now,
-					updateTime: now,
-					count: 1,
-					score: 50,
-					data: {}
-				};
 
-			if (!item) {
-				counter.inc();
-				cache.getEntity(entity.item, (err, text) => {
-					if (!err) item = text;
-					cache.getEntity(item.parents[1], (e, r) => {
-						if (e) return;
-						roomName = r.name;
-						counter.dec();
-					});
-				});
-			} else {
-				counter.inc();
-				cache.getEntity(item.parents[1], (e, r) => {
-					if (e) return;
-					roomName = r.name;
-					counter.dec();
-				});
-			}
+	return { creator, text, room, thread };
+}
 
-			counter.then(() => {
-				const title = roomName + ': ' + item.creator + ' mentioned you';
-				const urlLink = config.server.protocol + '//' + config.server.host + convertRouteToURL({
+async function createMention(relation): Promise<?Note> {
+	const { creator, text, room, thread } = await getItemsData(relation);
+	const now = Date.now();
+
+	if (
+		creator && creator.id &&
+		text && text.id &&
+		room && room.id &&
+		thread && thread.id
+	) {
+		const note: NoteType = {
+			id: '', // make flow happy
+			group: thread.id,
+			user: relation.user,
+			event: NOTE_MENTION,
+			createTime: now,
+			updateTime: now,
+			count: 1,
+			score: 50,
+			data: {
+				id: text.id,
+				creator: creator.id,
+				room: room.id,
+				thread: thread.id,
+				picture: `${server}/i/picture?user=${creator.id}&size=${128}`,
+				link: server + convertRouteToURL({
 					name: 'chat',
 					props: {
-						room: item.parents[1],
-						thread: item.parents[0],
-					},
-				});
-				noteObj.group = item.parents[0];
-				noteObj.data = {
-					id: item.id,
-					creator: item.creator,
-					body: item.body,
-					title,
-					type: Constants.NOTE_MENTION,
-					thread: entity.type === Constants.TYPE_TEXTREL ? item.parents[0] : null,
-					room: entity.type === Constants.TYPE_TEXTREL ? item.parents[1] : item.parents[0],
-					link: urlLink,
-					picture: `${config.server.protocol}//${config.server.host}/i/picture?user=${item.creator}&size=${48}`,
-				};
-				const	note = new Note(noteObj);
-				console.log('Note created: ', note);
-				changes.entities[note.id] = note;
-				counter1.dec();
-			});
+						room: room.id,
+						thread: thread.id,
+					}
+				}),
+				title: `${room.name}: ${creator.id} mentioned you`,
+				body: text.body,
+			},
+			type: TYPE_NOTE
+		};
+
+		return new Note(note);
+	}
+
+	return null;
+}
+
+function isMentioned(entity): boolean {
+	return entity.type === TYPE_TEXTREL && entity.roles && entity.roles.includes(ROLE_MENTIONED);
+}
+
+async function getNotesFromChanges(changes): Promise<Array<?Note>> {
+	const promises = [];
+
+	if (changes.entities) {
+		for (const id in changes.entities) {
+			const relation = changes.entities[id];
+
+			if (isMentioned(relation)) {
+				promises.push(createMention(relation));
+			}
 		}
 	}
-	counter1.then(next);
-	// next();
-}, Constants.APP_PRIORITIES.NOTE);
 
-log.info('Note module ready.');
+	return await Promise.all(promises);
+}
+
+async function addNotesToChanges(changes) {
+	const notes = await getNotesFromChanges(changes);
+
+	for (let i = 0, l = notes.length; i < l; i++) {
+		const note = notes[i];
+
+		if (note && note.id) {
+			changes.entities[note.id] = note;
+		}
+	}
+}
+
+bus.on('change', async (changes, next) => {
+	try {
+		await addNotesToChanges(changes);
+		next();
+	} catch (err) {
+		next(err);
+	}
+}, APP_PRIORITIES.NOTE);
