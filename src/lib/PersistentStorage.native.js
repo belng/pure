c4adaa39ac258eea5701ddb4d34fb515dc150980
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-04-30 20:23:12 +0530

    refactor(lib): add a PersistentStorage with object store for tests

---
+++ src/lib/PersistentStorage.native.js
@@ -0,0 +1,31 @@
+/* @flow */
+
+import ReactNative from 'react-native';
+
+const {
+	AsyncStorage
+} = ReactNative;
+
+export default class PersistentStorage {
+	_id: string;
+
+	constructor(id: string) {
+		this._id = id;
+	}
+
+	setItem(key: string, data: any): Promise<void> {
+		return AsyncStorage.setItem(this._getKeyWithPrefix(key), JSON.stringify(data));
+	}
+
+	getItem(key: string): Promise<any> {
+		return AsyncStorage.getItem(this._getKeyWithPrefix(key)).then(JSON.parse);
+	}
+
+	removeItem(key: string): Promise<void> {
+		return AsyncStorage.removeItem(this._getKeyWithPrefix(key));
+	}
+
+	_getKeyWithPrefix(key: string): string {
+		return `persistent_storage:${this._id}:${key}`;
+	}
+}
