fba2b2b7a6ed697db5692676c53ba472d1a1fb7d
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-05-07 03:19:02 +0530

    feat(ui): add a room list for moderation

---
+++ src/ui/components/containers/RoomListContainer.js
@@ -0,0 +1,85 @@
+/* @flow */
+
+import React, { Component, PropTypes } from 'react';
+import Connect from '../../../modules/store/Connect';
+import PassUserProp from '../../../modules/store/PassUserProp';
+import RoomList from '../views/Homescreen/RoomList';
+import { ROLE_FOLLOWER } from '../../../lib/Constants';
+
+const ITEM_LOADING = { type: 'loading' };
+
+const filterInvalidRels = data => data.map(result => {
+	if (!result.room || typeof result.room.type === 'string') {
+		return ITEM_LOADING;
+	}
+
+	if (!result.roomrel || typeof result.roomrel.type === 'string') {
+		return ITEM_LOADING;
+	}
+
+	return result;
+});
+
+const sortPlacesByTag = data => data.slice().sort((a, b) => {
+	if (a.room && a.room.tags && b.room && b.room.tags) {
+		const aTag = a.room.tags[0];
+		const bTag = b.room.tags[0];
+
+		if (aTag === bTag) {
+			return 0;
+		}
+
+		if (aTag > bTag) {
+			return -1;
+		}
+
+		if (aTag < bTag) {
+			return 1;
+		}
+	}
+
+	return -1;
+});
+
+const transformResults = data => sortPlacesByTag(filterInvalidRels(data));
+
+class RoomListContainer extends Component {
+	static propTypes = {
+		user: PropTypes.string.isRequired,
+	};
+
+	render() {
+		const { user } = this.props;
+
+		return (
+			<Connect
+				mapSubscriptionToProps={{
+					data: {
+						key: {
+							slice: {
+								type: 'roomrel',
+								link: {
+									room: 'item',
+								},
+								filter: {
+									user,
+									roles_cts: [ ROLE_FOLLOWER ],
+								},
+								order: 'createTime',
+							},
+							range: {
+								start: -Infinity,
+								end: Infinity,
+							},
+						},
+						transform: transformResults,
+					},
+				}}
+				passProps={this.props}
+				component={RoomList}
+			/>
+		);
+	}
+}
+
+export default PassUserProp(RoomListContainer);
