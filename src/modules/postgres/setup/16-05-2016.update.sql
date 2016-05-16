4b42e8b712144ade01d997fdf61c4bb25e7b2944
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-05-16 13:51:28 +0530

    chore(database): add script to update the db to latest schema

---
+++ src/modules/postgres/setup/16-05-2016.update.sql
@@ -0,0 +1,41 @@
+ALTER TABLE contacts ALTER COLUMN createtime SET NOT NULL;
+ALTER TABLE contacts ALTER COLUMN createtime SET DEFAULT extract(epoch from now());
+
+ALTER TABLE users ALTER COLUMN identities SET NOT NULL;
+ALTER TABLE users ALTER COLUMN createtime SET NOT NULL;
+ALTER TABLE users ALTER COLUMN createtime SET DEFAULT extract(epoch from now());
+
+ALTER TABLE items ALTER COLUMN parents SET NOT NULL;
+ALTER TABLE items ALTER COLUMN createtime SET NOT NULL;
+ALTER TABLE items ALTER COLUMN createtime SET DEFAULT extract(epoch from now());
+
+ALTER TABLE rooms ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE threads ALTER COLUMN name SET NOT NULL;
+ALTER TABLE threads ALTER COLUMN body SET NOT NULL;
+
+ALTER TABLE texts ALTER COLUMN body SET NOT NULL;
+
+ALTER TABLE rels ALTER COLUMN item SET NOT NULL;
+ALTER TABLE rels ALTER COLUMN "user" SET NOT NULL;
+ALTER TABLE rels ALTER COLUMN createtime SET NOT NULL;
+ALTER TABLE rels ALTER COLUMN createtime SET DEFAULT extract(epoch from now());
+
+ALTER TABLE notes ALTER COLUMN "group" SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN score SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN count SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN data SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN event SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN createtime SET NOT NULL;
+ALTER TABLE notes ALTER COLUMN count SET DEFAULT 1;
+ALTER TABLE notes ALTER COLUMN data SET DEFAULT '{}';
+ALTER TABLE notes ALTER COLUMN createtime SET DEFAULT extract(epoch from now());
+
+UPDATE users SET identities = '{}' WHERE identities IS NULL;
+UPDATE items SET parents = '{}' WHERE parents IS NULL;
+UPDATE rooms SET name = '' WHERE name IS NULL;
+UPDATE threads SET name = '' WHERE name IS NULL;
+UPDATE threads SET body = '' WHERE body IS NULL;
+UPDATE texts SET body = '' WHERE body IS NULL;
+UPDATE notes SET count = DEFAULT WHERE count IS NULL;
+UPDATE notes SET data = DEFAULT WHERE data IS NULL;
