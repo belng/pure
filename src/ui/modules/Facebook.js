a2c68ab47bcfb4114448b5ff84d99c7a1fc13172
Anup <allamsetty.anup@gmail.com>
2016-05-18 22:24:32 +0530

    Add modules for Facebook and Google Login on the web

--- src/ui/modules/Facebook.js
+++ src/ui/modules/Facebook.js
@@ -1,21 +1,40 @@
 /* @flow */
 
-type AccessToken = {
-	access_token: ?string;
-	user_id: ?string;
-	expires: ?number;
-	permissions_granted: Array<string>;
-	permissions_declined: Array<string>;
+import { config } from '../../core-client';
+
+type AuthCode = {
+	code: string;
 }
 
 export default class Facebook {
-	static logInWithReadPermissions: (permissions: Array<string>) => Promise<AccessToken>;
-	static logInWithPublishPermissions: (permissions: Array<string>) => Promise<AccessToken>;
-	static logOut: () => Promise<boolean>;
-	static getCurrentAccessToken: () => Promise<AccessToken>;
-	static sendGraphRequest: (
-		method: 'GET' | 'POST' | 'DELETE',
-		path: string,
-		params: { [key: string]: string }
-	) => void;
+	static logInWithReadPermissions(): Promise<AuthCode> {
+		return new Promise(resolve => {
+			function listener({ data }) {
+				if (data && data.type === 'auth' && data.provider === 'facebook') {
+					resolve({
+						code: data.code,
+					});
+					window.removeEventListener('message', listener);
+				}
+			}
+			window.addEventListener('message', listener);
+			window.open(config.server.protocol + '//' + config.server.host + config.facebook.login_url);
+		});
+	}
+
+	static logInWithPublishPermissions() {
+		return Promise.reject(new Error('Not implemented'));
+	}
+
+	static logOut() {
+		return Promise.resolve(true);
+	}
+
+	static getCurrentAccessToken() {
+		return Promise.reject(new Error('Not implemented'));
+	}
+
+	static sendGraphRequest() {
+		return Promise.reject(new Error('Not implemented'));
+	}
 }
