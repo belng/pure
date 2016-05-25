4508a7c9053b718daa983ad14262aa76ce070af3
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-05-25 05:43:28 +0530

    feat(web): add components for <Loading />, <Page />, <Splash /> and <Offline />

---
+++ src/ui/components/views/Splash.native.js
@@ -0,0 +1,45 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import ReactNative from 'react-native';
+import Colors from '../../Colors';
+import Loading from './Loading';
+
+const {
+	StatusBar,
+	StyleSheet,
+	View,
+	Image,
+} = ReactNative;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: Colors.primary,
+	},
+	logo: {
+		flex: 1,
+		resizeMode: 'contain',
+		marginTop: 240,
+	},
+	loading: {
+		height: 24,
+		width: 24,
+		marginVertical: 96,
+		marginHorizontal: 16,
+	},
+});
+
+export default class Splash extends Component<void, void, void> {
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusBar backgroundColor={Colors.primaryDark} />
+				<Image style={styles.logo} source={require('../../../../assets/logo-white.png')} />
+				<Loading style={styles.loading} />
+			</View>
+		);
+	}
+}
