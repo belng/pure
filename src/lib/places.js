9d33a6a9a937f6df03ba7d5095780027d04989cd
harish <harry.softer@gmail.com>
2016-05-27 11:26:55 +0530

    Move belong/place.js to lib

--- src/lib/places.js
+++ src/lib/places.js
@@ -1,6 +1,81 @@
 import { config } from '../core-server';
 import request from 'request';
 import winston from 'winston';
+import * as constants from '../lib/Constants';
+
+function getScore(types) {
+	let finalScore, finalTag = null;
+	for (const type of types) {
+		let score, tag;
+
+		switch (type) {
+		case 'administrative_area_level_3':
+			tag = constants.TAG_ROOM_CITY;
+			score = 9;
+			break;
+		case 'locality':
+			tag = constants.TAG_ROOM_CITY;
+			score = 10;
+			break;
+		case 'sublocality':
+			tag = constants.TAG_ROOM_AREA;
+			score = 20;
+			break;
+		case 'sublocality_level_4':
+		case 'sublocality_level_5':
+		case 'sublocality_level_3':
+		case 'sublocality_level_2':
+		case 'sublocality_level_1':
+			tag = constants.TAG_ROOM_AREA;
+			score = 20 + parseInt(type.substr(-1), 10);
+			break;
+		case 'colloquial_area':
+			tag = constants.TAG_ROOM_AREA;
+			score = 25;
+			break;
+		case 'neighborhood':
+			tag = constants.TAG_ROOM_AREA;
+			score = 26;
+			break;
+		case 'country':
+		case 'administrative_area_level_1':
+		case 'administrative_area_level_2':
+		case 'administrative_area_level_4':
+		case 'administrative_area_level_5':
+		case 'natural_feature':
+		case 'political':
+		case 'post_box':
+		case 'postal_code':
+		case 'postal_code_prefix':
+		case 'postal_code_suffix':
+		case 'postal_town':
+		case 'subpremise':
+		case 'floor':
+		case 'room':
+		case 'route':
+		case 'intersection':
+		case 'geocode':
+		case 'street_address':
+		case 'street_number':
+			score = 0;
+			tag = 0;
+			break; /* ignore these */
+		default:
+			tag = constants.TAG_ROOM_SPOT;
+			score = 30;
+		}
+
+		if (!finalScore || score > finalScore) {
+			finalScore = score;
+			finalTag = tag;
+		}
+	}
+
+	return {
+		score: finalScore,
+		tag: finalTag,
+	};
+}
 
 
 function handleError(message, reject) {
@@ -16,7 +91,7 @@ export function callApi(api: string, params: Object) {
 
 		Object.keys(params).map(name => name + '=' + params[name])
 		.join('&');
-		console.log("REQUEST:", p);
+
 		request(
 			p,
 			(error, response, body) => {
@@ -49,9 +124,77 @@ export function getNearByPlaces(lat: number, long: number, radius: number, type:
 		type
 	});
 }
-//
-// getNearByPlaces(-33.8670522, 151.1957362, 1000).then((res) => {
-// 	console.log(res.map(e=>e.geometry));
-// }).catch((e) => {
-// 	console.log(e);
-// });
+
+function placeToStub(place) {
+	const { tag } = getScore(place.types);
+	const stub = {
+		identity: 'place:' + place.place_id,
+		name: place.name ? place.name : place.address_components && place.address_components[0].long_name,
+		type: tag,
+		parents: place.parents,
+		meta: {
+			geometry: place.geometry,
+			photos: place.photos
+		}
+	};
+
+	if ('photos' in place) stub.meta.photos = place.photos;
+	return stub;
+}
+
+export function getStubset(placeid: string, rel: number): Promise<Object> {
+	let spot, score, tag;
+
+	return callApi('place/details', { placeid })
+	.then(place => {
+		const scoreTag = getScore(place.types);
+
+		spot = place;
+		score = scoreTag.score;
+		tag = scoreTag.tag;
+
+		return callApi('geocode', {
+			latlng: place.geometry.location.lat +
+				',' + place.geometry.location.lng,
+		});
+	})
+	.then(results => {
+		if (!score) score = 30;
+		let areas = results.filter(area => {
+			const res = getScore(area.types);
+			area.score = res.score;
+			return res.score && res.score < score;
+		});
+
+		spot.score = score;
+		let index = 0;
+
+		if (tag) {
+			for (const area of areas) {
+				if (area.place_id === placeid) break;
+				index++;
+			}
+
+			if (index === areas.length) areas.unshift(spot);
+		}
+
+		areas = areas.sort((a, b) => {
+			return a.score < b.score ? -1 : 1;
+		});
+
+		const parents = [];
+
+		for (let i = 0; i < areas.length; i++) {
+			areas[i].parents = [].concat(parents);
+			parents.push('place:' + areas[i].place_id);
+		}
+
+		areas = areas.map(placeToStub);
+
+		for (let i = areas.length - 1; i > 0; i--) {
+			areas[i].name += (areas[i - 1] ? ', ' + areas[i - 1].name : '');
+		}
+
+		return { rel, stubs: areas };
+	});
+}
