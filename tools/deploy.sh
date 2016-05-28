465c7df78892a3da8d281de7cbb2bea8fcad6bd3
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-05-28 14:13:04 +0530

    Give server address as parameter

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,6 +4,6 @@ if [[ "$1" == "" ]]; then
 	exit 1
 fi
 
-echo "Deploying branch $1"
+echo "Deploying branch $1 to $2"
 
-echo 'cd pure && git fetch origin && git checkout '$1' && export NODE_ENV="development" && echo $NODE_ENV && npm install && export NODE_ENV="production" && cd src/submodules/know && git checkout master && git pull && npm install && cd ../../.. && npm run build && sudo restart pure && tail -f logs/now.log' | ssh ubuntu@pure.heyneighbor.chat
+echo 'cd pure && git fetch origin && git checkout '$1' && export NODE_ENV="development" && echo $NODE_ENV && npm install && export NODE_ENV="production" && cd src/submodules/know && git checkout master && git pull && npm install && cd ../../.. && npm run build && sudo restart pure && tail -f logs/now.log' | ssh $2
--- tools/release.sh
+++ tools/release.sh
@@ -3,14 +3,14 @@
 #branch=$(ssh ubuntu@direct.stage.scrollback.io /home/ubuntu/scrollback/tools/create-hnbr-release-branch.sh)
 branch=$($HOME/pure/tools/create-release-branch.sh)
 if [[ $branch = "ERR_NOT_MASTER" ]]; then
-   echo "A release branch was not created because the staging server is not running master."
+   echo "A release branch was not created because the staging server is not running master. $branch"
    exit
 fi
 
-read -p "Release branch $branch created. Deploy to master (y/N)? " confirm
+read -p "Release branch $branch created. Deploy to producation server $1 (y/N)? " confirm
 if [[ $confirm = "y" ]]; then
    echo $branch 'will be deployed'
-   $HOME/pure/tools/./deploy.sh $branch
+   $HOME/pure/tools/./deploy.sh $branch $1
 else echo 'canceled'
 fi
 #echo new branch is: $branch
