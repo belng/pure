41f300b77dd922157a26b657dc7f3719dc9afab0
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-06-02 17:27:00 +0530

    Remove console logs

--- src/modules/score/score.js
+++ src/modules/score/score.js
@@ -15,14 +15,10 @@ function getScore(entity) {
 	upvote = 2 * Math.atan(upvote / 1) / Math.PI;
 	children = 2 * Math.atan(children / 5) / Math.PI;
 	follower = 2 * Math.atan(follower / 3) / Math.PI;
-
-	console.log('score module: ', entity);
-
 	const score = 0.7 * updateTime + 0.3 * createTime +
 	10 * upvote + 5 * children + 7.5 * follower +
 	1E9 * (entity.tags && entity.tags.indexOf(TAG_POST_STICKY) > -1 ? 1 : 0); // for pinned posts
-
-	console.log('score: ', score);
+	log.info('Score for thread: ', score);
 	return score;
 }
 
@@ -36,7 +32,6 @@ bus.on('change', (changes, next) => {
 		const entity = changes.entities[id];
 
 		if (entity.type === TYPE_THREAD) {
-			console.log('score module:1 ', entity);
 			counter.inc();
 			cache.getEntity(entity.id, (err, result) => {
 				if (err) {
@@ -44,10 +39,7 @@ bus.on('change', (changes, next) => {
 					return;
 				}
 				const newThread = jsonop.apply(entity, result || {});
-				console.log('result: ',result);
-				console.log('newThread: ', newThread)
 				entity.score = getScore(newThread);
-				console.log('score now: ', entity);
 				counter.dec();
 			});
 		}
