c7835a2bb999af16d97a113282c101f9708229c5
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-06-02 14:57:22 +0530

    fix(client): add misssing google analytics config

--- src/modules/client/ServerHTML.js
+++ src/modules/client/ServerHTML.js
@@ -52,23 +52,29 @@ const ServerHTML = ({
 				/>
 			)) : null}
 
-			{analytics.google ? (
-				<script dangerouslySetInnerHTML={{ __html: `
-					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
-					i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},
-					i[r].l=1*new Date();a=s.createElement(o),
-					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
-					m.parentNode.insertBefore(a,m)})(window,document,'script',
-					'https://www.google-analytics.com/analytics.js','ga');
+			{analytics.google ?
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;
+							i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},
+							i[r].l=1*new Date();a=s.createElement(o),
+							m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
+							m.parentNode.insertBefore(a,m)})(window,document,'script',
+							'https://www.google-analytics.com/analytics.js','ga');
 
-					ga('create', '${analytics.google}', 'auto');
-					ga('send', 'pageview');
-				`}}></script>
-			): null}
+							ga('create', '${analytics.google}', 'auto');
+							ga('send', 'pageview');
+							`
+					}}
+				></script> :
+				null
+			}
 
-			{analytics.optimizely ? (<script
-				src={`https://cdn.optimizely.com/js/${analytics.optimizely}.js`}
-			></script>) : null}
+			{analytics.optimizely ?
+				<script src={`https://cdn.optimizely.com/js/${analytics.optimizely}.js`}></script> :
+				null
+			}
 		</head>
 		<body>
 			<div id='root' dangerouslySetInnerHTML={{ __html: body }} />
