bcf5417e86438ea76e0ad894ee2483af99a21303
harish <harry.softer@gmail.com>
2016-06-10 18:16:03 +0530

    Set all updatetime values to current time

--- src/modules/postgres/setup/10-06-2016.update.sql
+++ src/modules/postgres/setup/10-06-2016.update.sql
@@ -1,3 +1,7 @@
+UPDATE users SET updateTime = extract(epoch from now())*1000;
+UPDATE items SET updateTime = extract(epoch from now())*1000;
+UPDATE rels SET updateTime = extract(epoch from now())*1000;
+
 ALTER TABLE users ALTER COLUMN updatetime SET DEFAULT extract(epoch from now())*1000;
 ALTER TABLE items ALTER COLUMN updatetime SET DEFAULT extract(epoch from now())*1000;
 ALTER TABLE rels ALTER COLUMN updatetime SET DEFAULT extract(epoch from now())*1000;
