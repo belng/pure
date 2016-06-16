6dbde41faeec1e33c2303c3279b55681a4082d34
harish <harry.softer@gmail.com>
2016-06-16 17:38:47 +0530

    Handle string createtime and updatetime in user model

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,14 @@ export default class User {
 
 		if (!data.id) { throw new Error('INVALID_USER_ID'); }
 
+		try {
+			if (data.createTime) data.createTime = parseInt(data.createTime);
+			if (data.updateTime) data.updateTime = parseInt(data.updateTime);
+		} catch (e) {
+			delete data.createTime;
+			delete data.updateTime;
+		}
+
 		for (const name of COLUMNS[Constants.TYPE_USER]) {
 			if (typeof data[name.toLowerCase()] !== 'undefined' || typeof data[name] !== 'undefined') {
 				this[name] = data[name] || data[name.toLowerCase()];
