7fa8aad0ba78e9b8c6a621fff75be3e35045d285
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-07-15 02:03:01 +0530

    Revert "fix(ui): don't use InteractionManager#cancel to avoid a crash due to unhandled rejection (#467)" (#487)
    This reverts commit 77a35a37379650e59656faa36ee014d833731f38.

--- src/ui/polyfills/requestIdleCallback.native.js
+++ src/ui/polyfills/requestIdleCallback.native.js
@@ -12,11 +12,8 @@ let id = 0;
 global.Promise.prototype.done = () => {}; // FIXME: Without this InteractionManager throws
 global.requestIdleCallback = (callback: Function) => {
 	const handle = id++;
-	pendingTasks[handle] = true;
-	InteractionManager.runAfterInteractions(() => {
-		if (pendingTasks[handle]) {
-			callback();
-		}
+	pendingTasks[handle] = InteractionManager.runAfterInteractions(() => {
+		callback();
 		delete pendingTasks[handle];
 	});
 	return handle;
@@ -24,6 +21,7 @@ global.requestIdleCallback = (callback: Function) => {
 
 global.cancelIdleCallback = (handle: number) => {
 	if (pendingTasks[handle]) {
+		pendingTasks[handle].cancel();
 		delete pendingTasks[handle];
 	}
 };
