47df9b8aceb7378b840a332fadc73ca3ae2e4b45
harish <harry.softer@gmail.com>
2016-07-21 19:11:08 +0530

    Add second strategy for url previews

---
+++ src/modules/url-preview/getContentType.js
@@ -0,0 +1,14 @@
+import request from 'request';
+
+export default async function(url: string): Promise<string> {
+	return new Promise((resolve) => {
+		request.get(url)
+		.on('response', response => {
+			if (response.status === 200) {
+				resolve(response.headers['content-type']);
+			} else {
+				resolve(null);
+			}
+		});
+	});
+}
