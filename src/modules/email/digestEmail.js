ae9ebec2df0aeb220496618ffb18c1bb0cbd068c
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-07-28 19:55:38 +0530

    New digest email template

--- src/modules/email/digestEmail.js
+++ src/modules/email/digestEmail.js
@@ -1,5 +1,5 @@
 /* eslint max-nested-callbacks: 0 */
-import getMailObj from './buildMailObj';
+// import getMailObj from './buildMailObj';
 import { config } from '../../core-server';
 import * as Constants from '../../lib/Constants';
 import Logger from '../../lib/logger';
@@ -16,11 +16,14 @@ const DIGEST_INTERVAL = 60 * 60 * 1000, /*DIGEST_DELAY = 24 * 60 * 60 * 1000,*/
 	connStr = config.connStr, conf = config.email, counter1 = new Counter(), log = new Logger(__filename);
 let lastEmailSent, /*end,*/ i = 0;
 const fields = [
-		'roomrels.presencetime',
 		'users.name',
+		'CAST(coalesce(rooms.counts ->> \'follower\' , \'0\') as integer) follower',
 		'threads.counts->>\'children\' children',
 		'users.identities', 'users.id userid',
 		'threads.score', 'threads.name threadtitle',
+		'threads.creator','threads.body',
+		'threads.meta->\'photo\'->>\'thumbnail_url\' photo',
+		'threads.counts->>\'upvote\' upvote',
 		'threads.createtime', 'threads.id threadid', 'rooms.name roomname',
 		'rooms.id roomid'
 	], joins = [
@@ -33,9 +36,12 @@ const fields = [
 		'users.id ~ &{range}',
 		'users.id=roomrels.user',
 		'threads.parents[1]=roomrels.item',
+		'(threads.counts->\'upvote\') IS NOT NULL',
+		'CAST(coalesce(threads.counts ->> \'upvote\' , \'0\') as integer) > 5',
 		'roomrels.item=rooms.id',
 		'threads.counts IS NOT NULL',
-		'threads.createtime >= extract(epoch from now()-interval \'1 days\')*1000',
+		'CAST(coalesce(threads.counts ->> \'children\' , \'0\') as integer) > 10',
+		'threads.createtime >= extract(epoch from now()-interval \'2 days\')*1000',
 		'(users.params-> \'email\'->>\'frequency\' = \'daily\' OR (users.params->\'email\') is null)',
 		'roles @> \'{3}\''
 	],
@@ -44,7 +50,7 @@ const fields = [
 			'SELECT ', pg.cat(fields, ', '),
 			'FROM ', pg.cat(joins, ', '),
 			'WHERE ', pg.cat(conditions, ' AND '),
-			'ORDER BY users.id'
+			'ORDER BY users.id, threads.score/CAST(coalesce(rooms.counts ->> \'follower\' , \'0\') as integer) desc'
 		]
 	);
 
@@ -53,27 +59,35 @@ function getSubject() {
 	return heading;
 }
 
-export function initMailSending (userRel: Object) {
-	const user = userRel.currentUser;
+export function initMailSending (userRel) {
+	// console.log(userRel)
+	for (let i=0; i<userRel.length; i++) {
+		if(i === 3) {
+			userRel[i].indexFourth = true;
+		}
+		if (i < 3) {
+			userRel[i].showBody = true;
+		}
+	}
+	const user = {id: userRel[0].userid, identities: userRel[0].identities};
 	if (!user.identities || !Array.isArray(user.identities)) {
 		log.info('No identities found for user: ', user);
 		return;
 	}
-	const	rels = userRel.currentRels/* .splice(0, 4)*/,
-		mailIds = user.identities.filter((el) => {
-			return /mailto:/.test(el);
-		});
+	mailIds = user.identities.filter((el) => {
+		return /mailto:/.test(el);
+	});
 
 	mailIds.forEach((mailId) => {
 		counter1.inc();
 		const emailAdd = mailId.slice(7),
-		emailSub = getSubject(rels),
+		emailSub = getSubject(),
 		date = Date.now();
 		const templateObj = {
 			token: jwt.sign({ email: emailAdd }, conf.secret, { expiresIn: '5 days' }),
 			domain: config.server.protocol + '//' + config.server.host,
 			link : '?utm_source=DailyDigest&utm_medium=Email&utm_term='+ encodeURIComponent(emailAdd) + '&utm_content=' + encodeURIComponent(emailSub) + '&utm_campaign=' + date,
-			rooms: rels,
+			threads: userRel,
 			email: emailAdd,
 			sub: emailSub,
 			date
@@ -107,7 +121,7 @@ function sendDigestEmail () {
 	let range;
 	const today = new Date().toString().split(" ")[2],
 		lastEmailSentDay = new Date(parseInt(lastEmailSent, 10)).toString().split(" ")[2];
-
+	//
 	if(new Date().getUTCHours() !== 22) { //Send at 3am IST. Move this to config
 		log.info('Not digest email time: ', new Date().getUTCHours());
 		return;
@@ -127,57 +141,27 @@ if (new Date().getDay() % 2 === 0) {
 }
 
 	log.info('Starting digest email');
-	// const startPoint = Date.now() - 30 * DIGEST_DELAY;
-	// let	start = /*lastEmailSent < startPoint ? lastEmailSent :*/ startPoint;
-	// end = Date.now() - 2 * DIGEST_DELAY;
-
-	// function getTimezone(hour) {
-	// 	const UtcHrs = new Date().getUTCHours(),
-	// 		c = UtcHrs > 12 ? 24 - UtcHrs : UtcHrs,
-	// 		d = hour - c,
-	// 		tz = d * 60,
-	// 		tzMin = tz - 30,
-	// 		tzMax = tz + 30;
-	//
-	// 	return { min: tzMin, max: tzMax };
-	// }
-	//
-	// const timeZone = getTimezone(conf.digestEmailTime);
-	//
-	// log.info('timezone: ', timeZone);
-	// if (conf.debug) {
-	// 	start = 0; end = Date.now();
-	// 	timeZone.min = 0;
-	// 	timeZone.max = 1000;
-	// }
-
-	// query.start = start;
-	// query.end = end;
-	// query.min = timeZone.min;
-	// query.max = timeZone.max;
 	query.range = range;
-	pg.readStream(config.connStr, query).on('row', (urel) => {
-		// log.info('Got user: ', urel.userid);
-		const emailObj = getMailObj(urel) || {};
-
-		if (Object.keys(emailObj).length !== 0) {
+	let threadRels = [], previousUser;
+	pg.readStream(config.connStr, query).on('row', (threadRel) => {
+		log.info('threadRel.userid: ', threadRel.userid, previousUser)
+		if(threadRel.userid !== previousUser && threadRels.length > 0) {
+			log.info('sending to user: ', previousUser);
 			++i;
-			log.info('send email to: ', i, emailObj.currentUser);
-			initMailSending(emailObj);
+			initMailSending(threadRels);
+			threadRels = [];
 		}
+		threadRels.push(threadRel);
+		previousUser = threadRel.userid;
 	}).on('end', () => {
-		const endUser = getMailObj({});
 		i++;
-		log.info('Sending to last user: ', endUser);
-		for (const j in endUser.currentRels) {
-			log.debug('c.currentRels[j].threads.length: ', endUser.currentRels[j].threads.length);
-		}
-		initMailSending(endUser);
-		// console.log('Got ', i, 'users');
+		log.info('Sending to last user: ', previousUser, threadRels);
+
+		initMailSending(threadRels);
 		log.info('ended digest email');
 		conf.sendConfirmEmailTo.forEach(email => {
 				send(conf.from, email, 'No. of Daily mails to Belong Users',
-				'Today Digest email has been sent to '+i+' users',
+				'Today Digest email has been sent to '+ i +' users',
 				e => {
 				if (!e) {
 					log.info('Confirmation email successfully sent');
