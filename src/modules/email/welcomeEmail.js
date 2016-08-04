ab5fecc2de43f1f54184585b68bfa58744bb1bd2
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-08-04 19:36:35 +0530

    New template for welcome email

--- src/modules/email/welcomeEmail.js
+++ src/modules/email/welcomeEmail.js
@@ -3,65 +3,38 @@ import fs from 'fs';
 import jwt from 'jsonwebtoken';
 import handlebars from 'handlebars';
 import * as pg from '../../lib/pg';
-import Counter from '../../lib/counter';
 import send from './sendEmail';
 import { config } from '../../core-server';
 import * as Constants from '../../lib/Constants';
-const log = new Logger(__filename);
+const log = new Logger(__filename, 'welcome');
 const WELCOME_INTERVAL = 5 * 60 * 1000, WELCOME_DELAY = 5 * 60 * 1000, connStr = config.connStr, conf = config.email,
 	template = handlebars.compile(fs.readFileSync(__dirname + '/../../../templates/' +
 	config.app_id + '.welcome.hbs', 'utf-8').toString());
 
 let lastEmailSent, end;
 
-function initMailSending(cUserRel) {
-	const counter = new Counter();
-
-	const user = cUserRel.user;
+function initMailSending(user) {
 	log.debug(user);
 	if (!user.identities || !Array.isArray(user.identities)) {
 		log.info('No identities found for user: ', user);
 		return;
 	}
-
-
-	const rels = cUserRel.rels,
-		mailIds = user.identities.filter((el) => {
+	const mailIds = user.identities.filter((el) => {
 			return /mailto:/.test(el);
-		});
+	});
 	mailIds.forEach((mailId) => {
-		counter.inc();
 		const emailAdd = mailId.slice(7);
 		log.info('Sending email to:', emailAdd);
 		const emailHtml = template({
-			user: user.id,
-			rels,
+			user: user.name || user.id,
 			domain: config.server.protocol + '//' + config.server.host + ':' + config.server.port,
 			token: jwt.sign({ email: emailAdd }, conf.secret, { expiresIn: '2 days' }),
 		});
 
-		send(conf.from, emailAdd, 'Welcome to ' + config.app_name, emailHtml, (e) => {
+		send(conf.from, emailAdd, 'Welcome to ' + config.app_name, emailHtml, e => {
 			if (e) {
 				log.error('Error in sending email');
-				counter.err(e);
-				return;
 			}
-			counter.dec();
-		});
-	});
-	counter.then((e) => {
-		if (e) {
-			log.info('Welcome email not sent', e);
-			return;
-		}
-		log.info('Welcome email successfully sent');
-		pg.write(connStr, [ {
-			$: 'UPDATE jobs SET lastrun=&{end} WHERE id=&{jid}',
-			end,
-			jid: Constants.JOB_EMAIL_WELCOME,
-		} ], (error) => {
-			lastEmailSent = end;
-			if (!error) log.info('successfully updated jobs for welcome email');
 		});
 	});
 }
@@ -75,27 +48,22 @@ function sendWelcomeEmail () {
 		end = Date.now();
 	}
 	pg.readStream(connStr, {
-		$: 'SELECT * FROM users WHERE createtime >&{start} AND createtime <= &{end}',
+		$: 'SELECT id, name, identities FROM users WHERE createtime > &{start} AND createtime <= &{end}',
 		start: lastEmailSent,
 		end,
-	}).on('row', (user) => {
+	}).on('row', user => {
 		log.info('Got a new user: ', user.id);
-		const userRel = {}, rels = [];
-
-		userRel.user = user;
-
-		pg.readStream(connStr, {
-			$: 'SELECT * FROM roomrels JOIN rooms ON item=id where "user" = &{user}',
-			user: user.id,
-		}).on('row', (rel) => {
-			rels.push(rel);
-		}).on('end', () => {
-			userRel.rels = rels;
-			log.info('sending email to user: ', userRel.user.id);
-			initMailSending(userRel);
-		});
+		initMailSending(user);
 	}).on('end', () => {
 		log.info('ended Welcome email');
+		pg.write(connStr, [{
+			$: 'UPDATE jobs SET lastrun=&{end} WHERE id=&{jid}',
+			end,
+			jid: Constants.JOB_EMAIL_WELCOME,
+		}], (error) => {
+			lastEmailSent = end;
+			if (!error) log.info('successfully updated jobs for welcome email');
+		});
 	});
 }
 
