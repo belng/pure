df81890a7e873b6df3350991330ca84e8f2efbc4
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-08-08 16:33:29 +0530

    Do a single query to get data for all users to send emails

--- src/modules/email/digestEmail.js
+++ src/modules/email/digestEmail.js
@@ -8,21 +8,22 @@ import * as pg from '../../lib/pg';
 import promisify from '../../lib/promisify';
 import jwt from 'jsonwebtoken';
 import send from './sendEmail';
+
 const DIGEST_INTERVAL = 60 * 60 * 1000,
 	template = handlebars.compile(
 		fs.readFileSync(__dirname + '/../../../templates/' + config.app_id + '.digest.hbs', 'utf-8').toString()
 	),
 	connStr = config.connStr, conf = config.email;
-let lastEmailSent, /*end,*/ log = new Logger(__filename, 'digest');
-const readSync = promisify(pg.read.bind(pg, connStr));
+let lastEmailSent; /* end,*/
+const log = new Logger(__filename, 'digest');
 
 export function initMailSending (userThreadRel) {
 	return new Promise(async res => {
 		const threads = userThreadRel.threadRels,
 			user = userThreadRel.user;
 
-		for (let i=0; i<threads.length; i++) {
-			if(i === 3) {
+		for (let i = 0; i < threads.length; i++) {
+			if (i === 3) {
 				threads[i].indexFourth = true;
 			}
 			if (i < 3) {
@@ -35,31 +36,31 @@ export function initMailSending (userThreadRel) {
 			res();
 		}
 
-		let mailIds = user.identities.filter((el) => {
+		const mailIds = user.identities.filter((el) => {
 			return /mailto:/.test(el);
 		});
-		if(mailIds.length === 0) res();
+		if (mailIds.length === 0) res();
 		const emailAdd = mailIds[0].slice(7),
-		emailSub = threads[0].threadtitle + ' & More from Belong';
+			emailSub = threads[0].threadtitle + ' & More from Belong';
 		const date = new Date().getDate(),
-			month = new Date().getMonth()+1,
+			month = new Date().getMonth() + 1,
 			year = new Date().getFullYear();
 		const formattedDate = date + '-' + month + '-' + year;
 		const templateObj = {
 			token: jwt.sign({ email: emailAdd }, conf.secret, { expiresIn: '5 days' }),
 			domain: config.server.protocol + '//' + config.server.host,
-			link : '?utm_source=DailyDigest&utm_medium=Email&utm_term='+ encodeURIComponent(user.id) + '&utm_content=' + encodeURIComponent(emailSub) + '&utm_campaign=' + encodeURIComponent(formattedDate),
+			link: '?utm_source=DailyDigest&utm_medium=Email&utm_term=' + encodeURIComponent(user.id) +
+			 '&utm_content=' + encodeURIComponent(emailSub) + '&utm_campaign=' + encodeURIComponent(formattedDate),
 			threads,
 			email: emailAdd,
-			sub: emailSub,
-			date
+			sub: emailSub
 		};
 		const	emailHtml = template(templateObj);
 		log.info('Digest email to: ', emailAdd);
 		const sendMail = promisify(send.bind());
-		try{
+		try {
 			await sendMail(conf.from, emailAdd, emailSub, emailHtml);
-		} catch(e) {
+		} catch (e) {
 			log.e(e);
 		}
 		res();
@@ -67,17 +68,17 @@ export function initMailSending (userThreadRel) {
 }
 
 async function sendDigestEmail () {
-	const today = new Date().toString().split(" ")[2],
-		lastEmailSentDay = new Date(parseInt(lastEmailSent, 10)).toString().split(" ")[2];
+	const today = new Date().toString().split(' ')[2],
+		lastEmailSentDay = new Date(parseInt(lastEmailSent, 10)).toString().split(' ')[2];
 
-	if(new Date().getUTCHours() !== 2) { //Send at 7am IST. Move this to config
+	if (new Date().getUTCHours() !== 2) { // Send at 7am IST. Move this to config
 		log.info('Not digest email time: ', new Date().getUTCHours());
 		return;
 	}
 
-	//Do not send digest email multiple times in the same day
+	// Do not send digest email multiple times in the same day
 	// This to prevent if server restarts multiple times in single hour. (cron job)
-	if ( today === lastEmailSentDay	) {
+	if (today === lastEmailSentDay) {
 		log.info('Digest email has been sent today. Last email sent time is: ', new Date((parseInt(lastEmailSent, 10))));
 		return;
 	}
@@ -89,49 +90,65 @@ async function sendDigestEmail () {
 	} else {
 		range = '^[p-z]|^[0-9]';
 	}
-	const users = await readSync({
-		$:`SELECT
-				id, identities, name FROM users
-				WHERE
-					presencetime IS NOT NULL
-					AND users.id ~ &{range}
-					AND (users.params-> 'email'->>'frequency' = 'daily' OR (users.params->'email') IS NULL)`,
-		range
-	});
-	log.info('Got ', users.length, ' users to send daily digest');
-	async function getRels()  {
-		for(let i=0; i< users.length; i++) {
-			log.info('Getting rels of: ', users[i].id);
-			let threadRels = await readSync({
-				$: `SELECT * FROM (SELECT
-					CAST(coalesce(rooms.counts ->> 'follower' , '0') as integer) follower, threads.counts->>'children' children, threads.score, threads.name threadtitle, threads.creator, threads.body, threads.meta->'photo'->>'thumbnail_url' photo, threads.score/(1+ 0.1*CAST(coalesce(rooms.counts ->> 'follower' , '0') as integer)) scoreperfollower, threads.counts->>'upvote' upvote, threads.createtime, threads.id threadid, rooms.name roomname, rooms.id roomid FROM  roomrels, threads, rooms
+	const puser = {};
+	let i, threadRels = [];
+	pg.readStream(connStr, {
+		$: `SELECT
+					users.id userid, users.identities, users.name username,
+					CAST(coalesce(rooms.counts ->> 'follower' , '0') as integer) follower,
+					threads.counts->>'children' children, threads.score,
+					threads.name threadtitle, threads.creator, threads.body,
+					threads.meta->'photo'->>'thumbnail_url' photo,
+					threads.score/(1+ 0.1*CAST(coalesce(rooms.counts ->> 'follower' , '0') as integer)) scoreperfollower,
+					threads.counts->>'upvote' upvote, threads.createtime,
+					threads.id threadid, rooms.name roomname, rooms.id roomid
+					FROM roomrels, threads, rooms, users
 						WHERE
-							roomrels.user=&{userid} AND threads.parents[1]=roomrels.item AND (threads.counts->'upvote') IS NOT NULL AND CAST(coalesce(threads.counts ->> 'upvote' , '0') as integer) > 1 AND roomrels.item=rooms.id AND threads.counts IS NOT NULL AND CAST(coalesce(threads.counts ->> 'children' , '0') as integer) > 5 AND threads.createtime >= extract(epoch from now()-interval '2 days')*1000 AND roles @> '{3}') as t WHERE creator NOT IN ('juhi', 'shreyaskutty', 'belong', 'belongbot')
-							ORDER BY scoreperfollower DESC`,
-				userid: users[i].id
-			});
-			threadRels.sort((a, b) => {
-				return b.upvote - a.upvote;
-			});
+							users.presencetime IS NOT NULL
+							AND users.id ~ &{range}
+							AND (users.params-> 'email'->>'frequency' = 'daily' OR (users.params->'email') IS NULL)
+							AND roomrels.user=users.id AND threads.parents[1]=roomrels.item
+							AND (threads.counts->'upvote') IS NOT NULL
+							AND CAST(coalesce(threads.counts ->> 'upvote' , '0') as integer) > 1
+							AND roomrels.item=rooms.id AND threads.counts IS NOT NULL
+							AND CAST(coalesce(threads.counts ->> 'children' , '0') as integer) > 1
+							AND threads.createtime >= extract(epoch from now()-interval '2 days')*1000
+							AND threads.creator NOT IN ('juhi', 'shreyaskutty', 'belong', 'belongbot')
+							AND roles @> '{3}'
+				ORDER BY users.id, scoreperfollower DESC limit 10`,
+		range
+	}).on('row', async row => {
+		// console.log(row);
+		if (row.userid !== puser.id && threadRels.length > 0) {
 			log.info('Got threads: ', threadRels.length);
-			if (threadRels.length === 0) {
-				continue;
-			}
+			++i;
 			threadRels = threadRels.slice(0, 8);
-			await initMailSending({threadRels, user: users[i]});
+			// await initMailSending({threadRels, user: puser});
+			threadRels = [];
 		}
-		log.info('ended digest email');
+		puser.id = row.userid;
+		puser.identities = row.identities;
+		puser.name = row.username;
+		delete row.userid;
+		delete row.identities;
+		delete row.username;
+		threadRels.push(row);
+
+	}).on('end', () => {
+		i++;
+		log.info('Sending meantion email to last user: ', puser, threadRels.length);
+		log.info('Got ', i++, ' users');
+		// initMailSending({threadRels, user: threadRels});
 		conf.sendConfirmEmailTo.forEach(email => {
-				send(conf.from, email, 'No. of Daily mails to Belong Users',
-				'Today Digest email has been sent to '+ users.length +' users',
+			send(conf.from, email, 'No. of Daily mails to Belong Users',
+				'Today Digest email has been sent to ' + i + ' users',
 				e => {
-				if (!e) {
-					log.info('Confirmation email successfully sent');
-				}
-			});
+					if (!e) {
+						log.info('Confirmation email successfully sent');
+					}
+				});
 		});
-	}
-	getRels();
+	});
 }
 
 export default function (row: Object) {
