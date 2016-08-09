16c584155f59d2d17dbadc56d84c93963c010530
harish <harry.softer@gmail.com>
2016-08-09 17:24:32 +0530

    Add the events module

---
+++ src/modules/events/events.js
@@ -0,0 +1,31 @@
+import * as pg from '../../lib/pg';
+import winston from 'winston';
+import { bus, config } from '../../core-server';
+
+function buildInsert(change) {
+	const events = change.events;
+	const insert = 'INSERT INTO events("type", "user", "data") VALUES';
+	const values = pg.cat(events.map(event => {
+		if (!event.user) event.user = change.auth.user;
+		return {
+			$: '(&{type}, &{user}, &{data}::jsonb)',
+			type: event.type,
+			user: event.user,
+			data: event.data
+		};
+	}), ',');
+
+	return pg.cat([
+		insert,
+		values,
+	], ' ');
+}
+bus.on('change', (change) => {
+	if (!change.events && change.events.length) return;
+
+	const sql = buildInsert(change);
+	pg.write(config.connStr, [ sql ], (err) => {
+		if (err) winston.error('Event Module: ', err.message);
+		else winston.info('Events registered');
+	});
+});
