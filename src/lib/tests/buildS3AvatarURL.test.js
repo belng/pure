46fcd1bab2dfba33b3ed6c86b8c038a144aed09a
Harish Kumar V <harishkumarv@users.noreply.github.com>
2016-08-11 17:28:42 +0530

    fix(image-upload): redirect url for avatar images (#565)

--- src/lib/tests/buildS3AvatarURL.test.js
+++ src/lib/tests/buildS3AvatarURL.test.js
@@ -2,16 +2,16 @@ import test from 'ava';
 import buildS3AvatarURL from '../buildS3AvatarURL';
 
 test('should return url when size is given', (t) => {
-	t.is(buildS3AvatarURL('subham', 32), 'https://s.bel.ng/a/subham/32.jpeg');
+	t.is(buildS3AvatarURL('subham', 32), 'https://s.bel.ng/a/subham/32.jpg');
 });
 
 test('should return url when size is not given', (t) => {
-	t.is(buildS3AvatarURL('subham'), 'https://s.bel.ng/a/subham/24.jpeg');
+	t.is(buildS3AvatarURL('subham'), 'https://s.bel.ng/a/subham/24.jpg');
 });
 
 test('should return url when invalid size is given', (t) => {
-	t.is(buildS3AvatarURL('subham', 156835), 'https://s.bel.ng/a/subham/960.jpeg');
-	t.is(buildS3AvatarURL('subham', 0), 'https://s.bel.ng/a/subham/16.jpeg');
-	t.is(buildS3AvatarURL('subham', 53), 'https://s.bel.ng/a/subham/64.jpeg');
-	t.is(buildS3AvatarURL('subham', 31), 'https://s.bel.ng/a/subham/32.jpeg');
+	t.is(buildS3AvatarURL('subham', 156835), 'https://s.bel.ng/a/subham/960.jpg');
+	t.is(buildS3AvatarURL('subham', 0), 'https://s.bel.ng/a/subham/16.jpg');
+	t.is(buildS3AvatarURL('subham', 53), 'https://s.bel.ng/a/subham/64.jpg');
+	t.is(buildS3AvatarURL('subham', 31), 'https://s.bel.ng/a/subham/32.jpg');
 });
