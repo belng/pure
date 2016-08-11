3cdccb191b6c7b3ab68a462b0149e27a385d501c
Chandrakanta Shatapathy <chandra@scrollback.io>
2016-08-11 14:05:19 +0530

    Use single query for digest email

--- src/modules/email/digestEmail.js
+++ src/modules/email/digestEmail.js
@@ -5,7 +5,6 @@ import Logger from '../../lib/logger';
 import fs from 'fs';
 import handlebars from 'handlebars';
 import * as pg from '../../lib/pg';
-import promisify from '../../lib/promisify';
 import jwt from 'jsonwebtoken';
 import send from './sendEmail';
 
@@ -17,36 +16,35 @@ const DIGEST_INTERVAL = 60 * 60 * 1000,
 let lastEmailSent; /* end,*/
 const log = new Logger(__filename, 'digest');
 
-export function initMailSending (userThreadRel) {
-	return new Promise(async res => {
-		const threads = userThreadRel.threadRels,
-			user = userThreadRel.user;
+function initMailSending (userThreadRel) {
+	const threads = userThreadRel.threadRels,
+		user = userThreadRel.user;
 
-		for (let i = 0; i < threads.length; i++) {
-			if (i === 3) {
-				threads[i].indexFourth = true;
-			}
-			if (i < 3) {
-				threads[i].showBody = true;
-			}
+	for (let i = 0; i < threads.length; i++) {
+		if (i === 3) {
+			threads[i].indexFourth = true;
 		}
-
-		if (!user.identities || !Array.isArray(user.identities)) {
-			log.info('No identities found for user: ', user);
-			res();
+		if (i < 3) {
+			threads[i].showBody = true;
 		}
+	}
 
-		const mailIds = user.identities.filter((el) => {
-			return /mailto:/.test(el);
-		});
-		if (mailIds.length === 0) res();
-		const emailAdd = mailIds[0].slice(7),
-			emailSub = threads[0].threadtitle + ' & More from Belong';
-		const date = new Date().getDate(),
-			month = new Date().getMonth() + 1,
-			year = new Date().getFullYear();
-		const formattedDate = date + '-' + month + '-' + year;
-		const templateObj = {
+	if (!user.identities || !Array.isArray(user.identities)) {
+		log.info('No identities found for user: ', user);
+		return;
+	}
+
+	const mailIds = user.identities.filter((el) => {
+		return /mailto:/.test(el);
+	});
+	if (mailIds.length === 0) return;
+	const emailAdd = mailIds[0].slice(7),
+		emailSub = threads[0].threadtitle + ' & More from Belong',
+		date = new Date().getDate(),
+		month = new Date().getMonth() + 1,
+		year = new Date().getFullYear(),
+		formattedDate = date + '-' + month + '-' + year,
+		templateObj = {
 			token: jwt.sign({ email: emailAdd }, conf.secret, { expiresIn: '5 days' }),
 			domain: config.server.protocol + '//' + config.server.host,
 			link: '?utm_source=DailyDigest&utm_medium=Email&utm_term=' + encodeURIComponent(user.id) +
@@ -54,16 +52,13 @@ export function initMailSending (userThreadRel) {
 			threads,
 			email: emailAdd,
 			sub: emailSub
-		};
-		const	emailHtml = template(templateObj);
-		log.info('Digest email to: ', emailAdd);
-		const sendMail = promisify(send.bind());
-		try {
-			await sendMail(conf.from, emailAdd, emailSub, emailHtml);
-		} catch (e) {
-			log.e(e);
+		},
+		emailHtml = template(templateObj);
+	log.info('Digest email to: ', emailAdd);
+	send(conf.from, emailAdd, emailSub, emailHtml, e => {
+		if (e) {
+			log.error(e);
 		}
-		res();
 	});
 }
 
@@ -91,7 +86,7 @@ async function sendDigestEmail () {
 		range = '^[p-z]|^[0-9]';
 	}
 	const puser = {};
-	let i, threadRels = [];
+	let i = 0, threadRels = [];
 	pg.readStream(connStr, {
 		$: `SELECT
 					users.id userid, users.identities, users.name username,
@@ -109,21 +104,21 @@ async function sendDigestEmail () {
 							AND (users.params-> 'email'->>'frequency' = 'daily' OR (users.params->'email') IS NULL)
 							AND roomrels.user=users.id AND threads.parents[1]=roomrels.item
 							AND (threads.counts->'upvote') IS NOT NULL
-							AND CAST(coalesce(threads.counts ->> 'upvote' , '0') as integer) > 1
+							AND CAST(coalesce(threads.counts ->> 'upvote' , '0') as integer) > 2
 							AND roomrels.item=rooms.id AND threads.counts IS NOT NULL
-							AND CAST(coalesce(threads.counts ->> 'children' , '0') as integer) > 1
+							AND CAST(coalesce(threads.counts ->> 'children' , '0') as integer) > 2
 							AND threads.createtime >= extract(epoch from now()-interval '2 days')*1000
 							AND threads.creator NOT IN ('juhi', 'shreyaskutty', 'belong', 'belongbot')
 							AND roles @> '{3}'
-				ORDER BY users.id, scoreperfollower DESC limit 10`,
+				ORDER BY users.id, scoreperfollower DESC`,
 		range
-	}).on('row', async row => {
+	}).on('row', row => {
 		// console.log(row);
 		if (row.userid !== puser.id && threadRels.length > 0) {
 			log.info('Got threads: ', threadRels.length);
 			++i;
 			threadRels = threadRels.slice(0, 8);
-			// await initMailSending({threadRels, user: puser});
+			initMailSending({ threadRels, user: puser });
 			threadRels = [];
 		}
 		puser.id = row.userid;
@@ -136,9 +131,10 @@ async function sendDigestEmail () {
 
 	}).on('end', () => {
 		i++;
-		log.info('Sending meantion email to last user: ', puser, threadRels.length);
-		log.info('Got ', i++, ' users');
-		// initMailSending({threadRels, user: threadRels});
+		log.info('Sending digest email to last user: ', puser, threadRels.length);
+		log.info('Got ', i, ' users');
+		threadRels = threadRels.slice(0, 8);
+		initMailSending({ threadRels, user: puser });
 		conf.sendConfirmEmailTo.forEach(email => {
 			send(conf.from, email, 'No. of Daily mails to Belong Users',
 				'Today Digest email has been sent to ' + i + ' users',
