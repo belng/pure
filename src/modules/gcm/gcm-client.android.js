f42d127b8f24b050a11d062b5884be213e110cdf
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-08-24 18:24:39 +0530

    feat(gcm): randomly set notification appearance (#582)

--- src/modules/gcm/gcm-client.android.js
+++ src/modules/gcm/gcm-client.android.js
@@ -4,8 +4,36 @@ import store from '../store/store';
 import GCM from '../../ui/modules/GCM';
 import Colors from '../../ui/Colors';
 
-const TITLE_TEMPLATE = '{{#roomNames.1}}{{#roomNames.0}}{{roomNames.0}}, {{roomNames.1}} and others{{/roomNames.0}}{{/roomNames.1}}{{^roomNames.1}}New activity in {{#roomNames.0}}{{roomNames.0}}{{/roomNames.0}}{{^roomNames.0}}Belong{{/roomNames.0}}{{/roomNames.1}}';
+const isSingle = Math.random() < 0.5;
+const titleTemplate = '{{#roomNames.1}}{{#roomNames.0}}{{roomNames.0}}, {{roomNames.1}} and others{{/roomNames.0}}{{/roomNames.1}}{{^roomNames.1}}New activity in {{#roomNames.0}}{{roomNames.0}}{{/roomNames.0}}{{^roomNames.0}}Belong{{/roomNames.0}}{{/roomNames.1}}';
 
+const singleTemplate = {
+	title: '{{items.0.data.title}}',
+	body: '<b>{{items.0.data.creator}}</b>: {{items.0.data.body}}',
+	picture: '{{{items.0.data.picture}}}',
+	link: '{{{items.0.data.link}}}',
+};
+
+const multiTemplate = {
+	title: `{{#single}}{{items.0.data.title}}{{/single}}{{^single}}${titleTemplate}{{/single}}`,
+	body: '<b>{{items.0.data.creator}}</b>: {{items.0.data.body}}',
+	picture: '{{#single}}{{{items.0.data.picture}}}{{/single}}',
+	link: '{{#single}}{{{items.0.data.link}}}{{/single}}{{^single}}/p/notes{{/single}}',
+	actions: [
+		{
+			label: '{{^single}}VIEW NOTIFICATIONS{{/single}}',
+			link: '/p/notes',
+			icon: 'ic_menu_forward.png',
+		},
+	],
+	style: {
+		title: titleTemplate,
+		line: '<b>{{item.data.creator}}</b>: {{item.data.body}}',
+		summary: '{{^single}}{{length}} new notifications{{/single}}',
+	},
+};
+
+GCM.cancelCurrentNotifications();
 GCM.markCurrentNotificationsAsRead();
 GCM.configureSchema({
 	event: 'number',
@@ -20,24 +48,14 @@ GCM.configureSchema({
 	group: 'string',
 });
 GCM.configureNotification({
-	style: 'inbox',
+	style: isSingle ? 'bigtext' : 'inbox',
 	sticky: false,
 	slient: true,
 	priority: 'normal',
 	category: 'social',
 	color: Colors.primary,
-	count: true,
-	template: {
-		title: `{{#single}}{{items.0.data.title}}{{/single}}{{^single}}${TITLE_TEMPLATE}{{/single}}`,
-		body: '<b>{{items.0.data.creator}}</b>: {{items.0.data.body}}',
-		picture: '{{#single}}{{{items.0.data.picture}}}{{/single}}',
-		link: '{{#single}}{{{items.0.data.link}}}{{/single}}{{^single}}/p/notes{{/single}}',
-		style: {
-			title: TITLE_TEMPLATE,
-			line: '<b>{{item.data.creator}}</b>: {{item.data.body}}',
-			summary: '{{^single}}{{length}} new notifications{{/single}}',
-		},
-	}
+	count: !isSingle,
+	template: isSingle ? singleTemplate : multiTemplate,
 });
 
 store.observe({ type: 'state', path: 'session', source: 'gcm' }).forEach(session => {
